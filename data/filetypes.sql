76e2ca332363412ddc5edac5de512b46765acbad
Enrico Tröger <enrico.troeger@uvena.de>
2006-02-10 20:39:53 +0000

    Initial import
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@177 ea778897-0a13-0410-b9d1-a72fbfd435f5

---
+++ data/filetypes.sql
@@ -0,0 +1,21 @@
+[styling]
+# foreground;background;bold
+default=0x000000,0xffffff,false
+comment=0x808080,0xffffff,false
+commentline=0x808080,0xffffff,false
+commentdoc=0x808080,0xffffff,false
+number=0x007F7F,0xffffff,false
+word=0x7F1A00,0xffffff,true
+word2=0x00007F,0xffffff,true
+string=0x7F007F,0xffffff,false
+character=0x000000,0xffffff,false
+operator=0x000000,0xffffff,true
+identifier=0x991111,0xffffff,false
+sqlplus=0x000000,0xffffff,false
+sqlplus_prompt=0x000000,0xffffff,false
+sqlplus_comment=0x000000,0xffffff,false
+quotedidentifier=0x991111,0xffffff,false
+
+[keywords]
+# all items must be in one line
+keywords=absolute action add admin after aggregate alias all allocate alter and any are array as asc assertion at authorization before begin binary bit blob boolean both breadth by call cascade cascaded case cast catalog char character check class clob close collate collation column commit completion connect connection constraint constraints  constructor continue corresponding create cross cube current current_date current_path current_role current_time current_timestamp current_user cursor cycle data date day deallocate dec decimal declare default deferrable deferred delete depth deref desc describe descriptor destroy destructor deterministic dictionary diagnostics disconnect distinct domain double drop dynamic each else end end-exec equals escape every except exception exec execute external false fetch first float for foreign found from free full function general get global go goto grant group grouping having host hour identity if ignore immediate in indicator initialize initially inner inout input insert int integer intersect interval into is isolation iterate join key language large last lateral leading left less level like limit local localtime localtimestamp locator map match minute modifies modify module month names national natural nchar nclob new next no none not null numeric object of off old on only open operation option or order ordinality out outer output pad parameter parameters partial path postfix precision prefix preorder prepare preserve primary prior privileges procedure public read reads real recursive ref references referencing relative restrict result return returns revoke right role rollback rollup routine row rows savepoint schema scroll scope search second section select sequence session session_user set sets size smallint some space specific specifictype sql sqlexception sqlstate sqlwarning start state statement static structure system_user table temporary terminate than then time timestamp timezone_hour timezone_minute to trailing transaction translation treat trigger true under union unique unknown unnest update usage user using value values varchar variable varying view when whenever where with without work write year zone
