cd89647bbbd45a0ee85d0710834cee43399f329a
Enrico Tröger <enrico.troeger@uvena.de>
2006-02-10 21:03:42 +0000

    added GEANY_FIFO_NAME
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@192 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/geany.h
+++ src/geany.h
@@ -55,6 +55,7 @@
 
 
 #define GEANY_HOME_DIR					g_get_home_dir()
+#define GEANY_FIFO_NAME					"geany_fifo.0"
 #define GEANY_CODENAME					"Nivers"
 #define GEANY_HOMEPAGE					"http://geany.uvena.de/"
 #define GEANY_MAX_OPEN_FILES			25
