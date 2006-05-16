3bb199a73852a34ccc6fa681022e8be4468a9987
Enrico Tröger <enrico.troeger@uvena.de>
2006-05-16 19:07:29 +0000

    Fixed folding bug
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@337 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- scintilla/LexBash.cxx
+++ scintilla/LexBash.cxx
@@ -627,7 +627,7 @@ static void FoldBashDoc(unsigned int startPos, int length, int, WordList *[],
                      && !IsCommentLine(lineCurrent+1, styler))
                 levelCurrent--;
         }
-		if (style == SCE_C_OPERATOR) {
+		if (style == SCE_SH_OPERATOR) {
 			if (ch == '{') {
 				levelCurrent++;
 			} else if (ch == '}') {
