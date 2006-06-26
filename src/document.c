add773c88cdb2e77b3337c5db2d42b4c02f34c08
Nick Treleaven <nick.treleaven@btinternet.com>
2006-06-26 15:49:07 +0000

    Fix a possible segfault on quitting caused by processing GTK messages before status messages
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@487 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/document.c
+++ src/document.c
@@ -525,16 +525,6 @@ int document_open_file(gint idx, const gchar *filename, gint pos, gboolean reado
 		utils_build_show_hide(idx);
 	}
 
-	// update sci to send SCN_UPDATEUI before we set status messages
-	while (gtk_events_pending())
-		gtk_main_iteration();
-	if (reload)
-		msgwin_status_add(_("File %s reloaded."), utf8_filename);
-	else
-		msgwin_status_add(_("File %s opened(%d%s)."),
-				utf8_filename, gtk_notebook_get_n_pages(GTK_NOTEBOOK(app->notebook)),
-				(readonly) ? _(", read-only") : "");
-
 	document_set_text_changed(idx);
 
 	g_free(data);
@@ -551,6 +541,15 @@ int document_open_file(gint idx, const gchar *filename, gint pos, gboolean reado
 		}
 		utils_update_recent_menu();
 	}
+
+	// currently status messages are overridden by update sci SCN_UPDATEUI message
+	// processing gtk messages first causes a segfault on closing & reloading utils.c
+	if (reload)
+		msgwin_status_add(_("File %s reloaded."), utf8_filename);
+	else
+		msgwin_status_add(_("File %s opened(%d%s)."),
+				utf8_filename, gtk_notebook_get_n_pages(GTK_NOTEBOOK(app->notebook)),
+				(readonly) ? _(", read-only") : "");
 
 	g_free(utf8_filename);
 	g_free(locale_filename);
