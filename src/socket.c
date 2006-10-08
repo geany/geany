ac2a9ed1e4035f5bfed8cc0df16349d062788eb7
Enrico Tröger <enrico.troeger@uvena.de>
2006-10-08 21:43:54 +0000

    Added additional header files for compatibility with FreeBSD.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@873 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/socket.c
+++ src/socket.c
@@ -26,6 +26,9 @@
 #ifdef HAVE_SOCKET
 
 #ifndef G_OS_WIN32
+# include <string.h>
+# include <sys/time.h>
+# include <sys/types.h>
 # include <sys/socket.h>
 # include <sys/un.h>
 # include <netinet/in.h>
