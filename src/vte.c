ac48b2bc35a1698ecbd3833453ec55960fbcad2b
Enrico Tröger <enrico.troeger@uvena.de>
2007-01-15 15:08:10 +0000

    Fixed changing directories when the path contains whitespace.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@1192 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/vte.c
+++ src/vte.c
@@ -436,13 +436,12 @@ void vte_cwd(const gchar *filename, gboolean force)
 	if (vte_info.have_vte && (vc->follow_path || force) && filename != NULL)
 	{
 		gchar *path;
-		gchar *cmd;
 
 		path = g_path_get_dirname(filename);
 		vte_get_working_directory();	// refresh vte_info.dir
 		if (! utils_str_equal(path, vte_info.dir))
 		{
-			cmd = g_strconcat("cd ", path, "\n", NULL);
+			gchar *cmd = g_strconcat("cd \"", path, "\"\n", NULL);
 			vte_send_cmd(cmd);
 			g_free(cmd);
 		}
