011fcde8eac75efa42f0ea01ff2ebd9cd1305952
Enrico Tröger <enrico.troeger@uvena.de>
2007-05-21 15:24:27 +0000

    Updated for Geany 0.11.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@1551 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- win32-config.h
+++ win32-config.h
@@ -286,7 +286,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "0.10"
+#define VERSION "0.11"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
