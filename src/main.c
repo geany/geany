56cbbea656cf0dd193f831b3c5dac97f6bda3f24
Enrico Tröger <enrico.troeger@uvena.de>
2007-06-04 13:22:43 +0000

    Add rpl_malloc() for better compatibility with non-GNU systems (thanks to Harri Koskinen for reporting).
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@1596 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/main.c
+++ src/main.c
@@ -846,3 +846,16 @@ void main_quit()
 
 	gtk_main_quit();
 }
+
+
+// malloc compatibility code
+#undef malloc
+void *malloc(size_t n);
+
+// Allocate an N-byte block of memory from the heap. If N is zero, allocate a 1-byte block.
+void *rpl_malloc(size_t n)
+{
+	if (n == 0)
+		n = 1;
+	return malloc(n);
+}
