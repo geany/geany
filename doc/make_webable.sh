38b5dbe53ad8a126de789c9884a55fd71cdafe04
Enrico Tröger <enrico.troeger@uvena.de>
2007-08-09 10:05:00 +0000

    Remove all docbook related files and use reST to generate the documentation. Thanks to John Gabriele for his great work on converting from docbook to the reST format.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@1774 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- doc/make_webable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# this converts the UTF-8 string in the Content-Type meta tag in all
-# html files to lower case, otherwise some browsers
-# (e.g. Mozilla Firefox) have problems to detect the right encoding
-# if it is put through a webserver
-
-sed -i 's/charset=UTF-8/charset=utf-8/g' html/*.html
