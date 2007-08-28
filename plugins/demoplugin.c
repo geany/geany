4c3d5d807253bd584339379d163dcff4e500dd33
Enrico Tröger <enrico.troeger@uvena.de>
2007-08-28 10:48:52 +0000

    Fix comment for PLUGIN_INFO.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@1831 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- plugins/demoplugin.c
+++ plugins/demoplugin.c
@@ -37,7 +37,7 @@ GeanyData		*geany_data;
  * for binary compatibility. */
 VERSION_CHECK(7)
 
-/* All plugins must set name and description */
+/* All plugins must set name, description and version */
 PLUGIN_INFO(_("Demo"), _("Example plugin."), "0.1")
 
 
