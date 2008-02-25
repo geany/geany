4a62d1d04fc7062463525ad78b60c278d8000a5e
Nick Treleaven <nick.treleaven@btinternet.com>
2008-02-25 17:02:23 +0000

    When closing a tab when using left-to-right tabs, focus the next document, not the previous.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@2282 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/notebook.c
+++ src/notebook.c
@@ -406,11 +406,22 @@ notebook_tab_close_clicked_cb(GtkButton *button, gpointer user_data)
 }
 
 
-// Always use this instead of gtk_notebook_remove_page().
+/* Always use this instead of gtk_notebook_remove_page(). */
 void notebook_remove_page(gint page_num)
 {
+	gint oldpage = gtk_notebook_get_current_page(GTK_NOTEBOOK(app->notebook));
+
 	gtk_notebook_remove_page(GTK_NOTEBOOK(app->notebook), page_num);
+
 	tab_count_changed();
+
+	/* Focus the next page, not the previous */
+	if (oldpage == page_num && prefs.tab_order_ltr)
+	{
+		/* Unless the first tab was closed */
+		if (oldpage > 0)
+			gtk_notebook_set_current_page(GTK_NOTEBOOK(app->notebook), oldpage);
+	}
 }
 
 
