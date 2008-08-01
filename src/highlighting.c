ee3db9554a45afd43013fd92a4db1143bff29491
Enrico Tröger <enrico.troeger@uvena.de>
2008-08-01 15:56:37 +0000

    Add some keywords for filetype SQL (patch by Felipe Pena, thanks).
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@2850 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/highlighting.c
+++ src/highlighting.c
@@ -2140,31 +2140,31 @@ static void styleset_sql_init(gint ft_id, GKeyFile *config, GKeyFile *config_hom
 	get_keyfile_keywords(config, config_home, "keywords", "keywords", GEANY_FILETYPES_SQL, 0,
 						"absolute action add admin after aggregate \
 						alias all allocate alter and any are array as asc \
-						assertion at authorization before begin binary bit blob boolean both breadth by \
-						call cascade cascaded case cast catalog char character check class clob close collate \
+						assertion at authorization before begin bfile bigint binary bit blob bool boolean both breadth by \
+						call cascade cascaded case cast catalog char character check class clob close cluster collate \
 						collation column commit completion connect connection constraint constraints \
 						constructor continue corresponding create cross cube current \
 						current_date current_path current_role current_time current_timestamp \
 						current_user cursor cycle data date day deallocate dec decimal declare default \
 						deferrable deferred delete depth deref desc describe descriptor destroy destructor \
-						deterministic dictionary diagnostics disconnect distinct domain double drop dynamic \
-						each else end end-exec equals escape every except exception exec execute external \
-						false fetch first float for foreign found from free full function general get global \
-						go goto grant group grouping having host hour identity if ignore immediate in indicator \
+						deterministic diagnostics dictionary dimension disconnect diskgroup distinct domain double drop dynamic \
+						each else end end-exec equals escape every except exception exec execute exists explain external \
+						false fetch first fixed flashback float for foreign found from free full function general get global \
+						go goto grant group grouping having host hour identity if ignore immediate in index indextype indicator \
 						initialize initially inner inout input insert int integer intersect interval \
 						into is isolation iterate join key language large last lateral leading left less level like \
-						limit local localtime localtimestamp locator map match minute modifies modify module month \
-						names national natural nchar nclob new next no none not null numeric object of off old on only \
-						open operation option or order ordinality out outer output pad parameter parameters partial path \
-						postfix precision prefix preorder prepare preserve primary prior privileges procedure public \
-						read reads real recursive ref references referencing relative restrict result return returns \
-						revoke right role rollback rollup routine row rows savepoint schema scroll scope search \
-						second section select sequence session session_user set sets size smallint some space \
-						specific specifictype sql sqlexception sqlstate sqlwarning start state statement static \
-						structure system_user table temporary terminate than then time timestamp \
-						timezone_hour timezone_minute to trailing transaction translation year zone\
-						treat trigger true under union unique unknown unnest update usage user using \
-						value values varchar variable varying view when whenever where with without work write");
+						limit local localtime localtimestamp locator long map match materialized mediumblob mediumint mediumtext \
+						merge middleint minus minute modifies modify module month names national natural nchar nclob new next no noaudit \
+						none not null numeric nvarchar2 object of off old on only open operation option or order ordinality out outer \
+						output package pad parameter parameters partial path postfix precision prefix preorder prepare preserve primary \
+						prior privileges procedure profile public purge read reads real recursive ref references referencing regexp regexp_like \
+						relative rename restrict result return returning returns revoke right role rollback rollup routine row rows \
+						savepoint schema scroll scope search second section select sequence session session_user set sets size \
+						smallint some space specific specifictype sql sqlexception sqlstate sqlwarning start state statement static \
+						structure synonym system_user table tablespace temporary terminate than then time timestamp \
+						timezone_hour timezone_minute tinyint to trailing transaction translation  treat trigger true truncate \
+						type under union unique unknown unnest update usage user using value values varchar varchar2 variable varying \
+						view when whenever where with without work write year zone");
 	style_sets[GEANY_FILETYPES_SQL].keywords[1] = NULL;
 
 	get_keyfile_wordchars(config, config_home,
