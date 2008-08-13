e78b6a9b689c34af53df823dc31581a30244625c
Enrico Tröger <enrico.troeger@uvena.de>
2008-08-13 18:40:34 +0000

    Replace fprintf() with g_critical().
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@2884 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/support.c
+++ src/support.c
@@ -118,7 +118,7 @@ create_pixbuf                          (const gchar     *filename)
   pixbuf = gdk_pixbuf_new_from_file (pathname, &error);
   if (!pixbuf)
     {
-      fprintf (stderr, "Failed to load pixbuf file: %s: %s\n",
+      g_critical ("Failed to load pixbuf file: %s: %s\n",
                pathname, error->message);
       g_error_free (error);
     }
