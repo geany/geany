33d207e27da4795b38d1c3ce54dd1f2974d2b522
Nick Treleaven <nick.treleaven@btinternet.com>
2008-08-26 15:18:36 +0000

    Add script to add a file to SVN, setting the correct properties and warning about updating po/POTFILES.in.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@2904 ea778897-0a13-0410-b9d1-a72fbfd435f5

---
+++ scripts/svn-add.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+FILE=$1
+if [[ -n $FILE ]]; then
+	svn add $FILE
+	svn propset svn:keywords 'Author Date Id Revision' $FILE
+	svn propset svn:eol-style native $FILE
+fi
+echo '>>> Remember to update po/POTFILES.in (if necessary) <<<'
