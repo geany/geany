55a40710aba64722e5fe47ee00a0078c460d3156
Enrico Tröger <enrico.troeger@uvena.de>
2008-09-08 14:27:22 +0000

    Remove bashisms. Add support for passing asterisk wildcards to missing-mnemonics.sh.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@2928 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- scripts/missing-mnemonics.sh
+++ scripts/missing-mnemonics.sh
@@ -3,10 +3,12 @@
 # License:	GPL V2 or later
 # Usage:	check-mnemonics.sh [file list]
 
-if [[ $1 == '' ]]; then
-	FILES='src/*.c plugins/*.c'
+
+if [ $# -ge 1 ]; then
+	FILES=$@
 else
-	FILES=$1
+	FILES='src/*.c plugins/*.c'
 fi
+
 
 fgrep -n 'menu_item_new' $FILES |egrep -v '".*_[a-zA-Z0-9]' |fgrep -v from_stock |fgrep -v '_("No custom commands defined.")' |fgrep -vi '_("invisible")' |egrep '_\(".+' --color
