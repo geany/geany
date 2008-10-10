5249fde4323ad84ed273a899873ecce94e634be4
Enrico Tröger <enrico.troeger@uvena.de>
2008-10-10 16:35:25 +0000

    Fix for the last fix: do it the proper way by ignoring any modifiers we don't want, only check for Ctrl, Alt and Shift.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@3064 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/editor.c
+++ src/editor.c
@@ -193,12 +193,14 @@ on_editor_button_press_event           (GtkWidget *widget,
 	editor_info.click_pos = sci_get_position_from_xy(doc->editor->sci, (gint)event->x, (gint)event->y, FALSE);
 	if (event->button == 1)
 	{
+		guint state = event->state & GEANY_KEYS_MODIFIER_MASK;
+
 		if (event->type == GDK_BUTTON_PRESS && editor_prefs.disable_dnd)
 		{
 			gint ss = sci_get_selection_start(doc->editor->sci);
 			sci_set_selection_end(doc->editor->sci, ss);
 		}
-		if (event->type == GDK_BUTTON_PRESS && event->state & GDK_CONTROL_MASK)
+		if (event->type == GDK_BUTTON_PRESS && state == GDK_CONTROL_MASK)
 		{
 			sci_set_current_position(sci, editor_info.click_pos, FALSE);
 			keybindings_send_command(GEANY_KEY_GROUP_GOTO, GEANY_KEYS_GOTO_TAGDEFINITION);
