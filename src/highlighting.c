aebe1b346c68feff377fc4b2d1761dd54823f73b
Enrico Tröger <enrico.troeger@uvena.de>
2008-10-15 14:14:48 +0000

    Fix again colouring for filetype None.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@3098 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/highlighting.c
+++ src/highlighting.c
@@ -1910,13 +1910,10 @@ static void styleset_none(ScintillaObject *sci)
 {
 	SSM(sci, SCI_SETLEXER, SCLEX_NULL, 0);
 
-	styleset_common(sci);
-
+	/* we need to set STYLE_DEFAULT before we call SCI_STYLECLEARALL in styleset_common() */
 	set_sci_style(sci, STYLE_DEFAULT, GEANY_FILETYPES_NONE, GCS_DEFAULT);
 
-	/* we need this to clear any other styles than STYLE_DEFAULT, otherwise the
-	 * "invert_all" option breaks */
-	SSM(sci, SCI_STYLECLEARALL, 0, 0);
+	styleset_common(sci);
 
 	SSM(sci, SCI_SETWORDCHARS, 0, (sptr_t) common_style_set.wordchars);
 	SSM(sci, SCI_SETWHITESPACECHARS, 0, (sptr_t) whitespace_chars);
