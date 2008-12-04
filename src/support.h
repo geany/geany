66d276003e738634bb332df0e8ed31017a61f705
Enrico Tröger <enrico.troeger@uvena.de>
2008-12-04 15:56:33 +0000

    Removed unused code.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@3314 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/support.h
+++ src/support.h
@@ -45,25 +45,3 @@
  */
 GtkWidget*  lookup_widget              (GtkWidget       *widget,
                                         const gchar     *widget_name);
-
-
-/* Use this function to set the directory containing installed pixmaps. */
-void        add_pixmap_directory       (const gchar     *directory);
-
-
-/*
- * Private Functions.
- */
-
-/* This is used to create the pixmaps used in the interface. */
-GtkWidget*  create_pixmap              (GtkWidget       *widget,
-                                        const gchar     *filename);
-
-/* This is used to create the pixbufs used in the interface. */
-GdkPixbuf*  create_pixbuf              (const gchar     *filename);
-
-/* This is used to set ATK action descriptions. */
-void        glade_set_atk_action_description (AtkAction       *action,
-                                              const gchar     *action_name,
-                                              const gchar     *description);
-
