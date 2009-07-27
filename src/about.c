913c8ca806d617ea475efe8f8efbb6b78068865e
Frank Lanitz <frank@frank.uvena.de>
2009-07-27 19:33:09 +0000

    Add Slovenian translation
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@4031 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/about.c
+++ src/about.c
@@ -64,6 +64,7 @@ const gchar *translators[][2] = {
 	{ "pt_BR", "Alexandra Moreire &lt;alexandream@gmail.com&gt;\nAdrovane Marques Kade &lt;adrovane@gmail.com&gt;" },
 	{ "ro", "Alex Eftimie &lt;alex@rosedu.org&gt;" },
 	{ "ru_RU", "brahmann_ &lt;brahmann@pisem.net&gt;,\nNikita E. Shalaev &lt;nshalaev@eu.spb.ru&gt;" },
+	{ "sl_SI", "Jože Klepec &lt;joze.klepec@siol.net&gt;"},
 	{ "sv", "Tony Mattsson &lt;superxorn@gmail.com&gt;" },
 	{ "tr", "Gürkan Gür &lt;seqizz@gmail.com&gt;"},
 	{ "uk", "Boris Dibrov &lt;dibrov.bor@gmail.com&gt;" },
