aa85bcec6aa59450396c3865e4dd1ea69f0d0ecc
Nick Treleaven <nick.treleaven@btinternet.com>
2009-08-20 12:16:28 +0000

    Add geanyfunctions.h to API docs.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@4112 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- plugins/geanyfunctions.h
+++ plugins/geanyfunctions.h
@@ -1,10 +1,16 @@
-/* This file is generated automatically by genapi.py - do not edit.
+/* This file is generated automatically by genapi.py - do not edit. */
+
+/** @file geanyfunctions.h @ref geany_functions wrappers.
+ * This allows the use of normal API function names in plugins by defining macros.
  *
- * @file geanyfunctions.h @ref geany_functions wrappers.
- * This allows the use of normal API function names in plugins.
+ * E.g.:@code
+ * #define plugin_add_toolbar_item \
+ * 	geany_functions->p_plugin->add_toolbar_item @endcode
+ *
  * You need to declare the @ref geany_functions symbol yourself.
  *
- * Note: This must be included after all other API headers.
+ * Note: This must be included after all other API headers to prevent conflicts with
+ * other header's function prototypes - this is done for you when using geanyplugin.h.
  */
 
 #ifndef GEANY_FUNCTIONS_H
