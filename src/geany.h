5bda02fd5ebf7f6754104f200b290f63c6c7fd60
Nick Treleaven <nick.treleaven@btinternet.com>
2009-08-27 14:02:56 +0000

    Define G_GNUC_WARN_UNUSED_RESULT if GLib < 2.10.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@4135 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/geany.h
+++ src/geany.h
@@ -91,4 +91,9 @@ extern gboolean	ignore_callback;
 /* prototype is here so that all files can use it. */
 void geany_debug(gchar const *format, ...) G_GNUC_PRINTF (1, 2);
 
+
+#ifndef G_GNUC_WARN_UNUSED_RESULT
+#define G_GNUC_WARN_UNUSED_RESULT
+#endif
+
 #endif
