abb0f8c24470ae9980470265e5fd1dfb6b0be835
Enrico Tröger <enrico.troeger@uvena.de>
2010-01-31 21:51:13 +0000

    Backport from trunk: Update partly outdated and wrong doc comment.
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/branches/geany-0.18.1@4619 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- tagmanager/include/tm_tagmanager.h
+++ tagmanager/include/tm_tagmanager.h
@@ -28,39 +28,7 @@
  TagManager is <a href="http://www.gnu.org/philosophy/free-sw.html">free software</a>,
  licenced under the <a href="http://www.gnu.org/licenses/gpl.html">GPL</a>. You can only
  use it with free software (GPL compatible) projects. This is chiefly because it uses
- code from ctags which is under GPL. I plan to replace the ctags part with a custom parser
- in the future in which case it will be placed under LGPL. If you want to use it with
- a commercial project, feel free to contribute a C/C++/Java parser.
- \section Hacking
- TagManager is tested to work on Linux. You need a basic GNOME 1.4 installation and
- the auto tools (autoconf, automake, etc) if you want to configure it to your taste.
- <a href="http://anjuta.sourceforge.net/">Anjuta</a> 0.1.7 or above is recommended
- if you want to hack on the sources. If you simply plan to use it, only GLib is required.
- \section Installation
- TagManager can be installed using the standard UNIX method, i.e.:
- 
- 	-# tar zxvf TagManager-[Version].tar.gz
- 	-# cd TagManager-[Version]
- 	-# ./configure [configure options]
-	-# make
- 
- Currently, 'make install' will not do anything since TagManager is meant to be
- included statically in projects. This might change in the future.
- \section Usage
- I have tried to make the API as simple as possible. Memory allocation/deallocation
- is mostly automatic, so you shouldn't have to directly malloc() or free()
- anything. The API is pseudo-OO, similar is structure to the GTK+ API. This document
- provides a good reference to the API. However, if you want to see real-world usage
- of the API, then the utility programs are the best place to start. Currently, there
- are three such programs in the 'tests' subdirectory, namely tm_tag_print.c,
- tm_project_test.c and tm_global_tags.c. It might be a good idea if you go through the
- code for these programs once before starting to use the tag manager library.
- \section Limitations
- Currently, only C, C++ and Java are supported. Adding other languages should be easy
- but I did not require them so they are missing. Tag Manager is also MT-unsafe - this is
- a deliberate design decision since making it MT-safe did not seem to be very useful
- to me and it slows things down. Previous versions has rudimentary code to support MT
- operations but they have been removed since version 0.5.
+ code from ctags which is under GPL.
 */
 
 /*! \file
