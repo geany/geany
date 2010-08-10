47e37994ec94090ba30dc871df1dcba7886aba07
Nick Treleaven <nick.treleaven@btinternet.com>
2010-08-10 16:41:10 +0000

    Add reminder for geany.nsi ;-)
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@5126 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- scripts/svn-add.sh
+++ scripts/svn-add.sh
@@ -5,4 +5,4 @@ if [ -n "$FILES" ]; then
 	svn propset svn:keywords 'Author Date Id Revision' $FILES
 	svn propset svn:eol-style native $FILES
 fi
-echo '>>> Remember to update Makefile.am, makefile.win32, wscript, po/POTFILES.in (if necessary) <<<'
+echo '>>> Remember to update Makefile.am, makefile.win32, wscript, po/POTFILES.in, geany.nsi (if necessary) <<<'
