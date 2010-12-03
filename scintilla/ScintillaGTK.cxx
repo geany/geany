2982f223371c7000622369c67405159bb36118ea
Nick Treleaven <nick.treleaven@btinternet.com>
2010-12-03 13:51:54 +0000

    Backport Scintilla HG fix: Remove styling idle in destructor to avoid crashing when idle called after ScintillaGTK instance destroyed (fixes #3111058).
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@5462 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- scintilla/ScintillaGTK.cxx
+++ scintilla/ScintillaGTK.cxx
@@ -355,6 +355,7 @@ ScintillaGTK::ScintillaGTK(_ScintillaObject *sci_) :
 }
 
 ScintillaGTK::~ScintillaGTK() {
+	g_idle_remove_by_data(this);
 }
 
 void ScintillaGTK::RealizeThis(GtkWidget *widget) {
