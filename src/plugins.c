geany/geany

commit 8e79a2255ac8b2675881fe7879c305bff957a2e1

Prefer prepend elements to lists rather than append them
This is for better performances since appending to a list means walking
it to find the last element to append to. When the list ordering
matters, simply reverse the list after prepengins.

git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@5586 ea778897-0a13-0410-b9d1-a72fbfd435f5

src/plugins.c
@@ -608,7 +608,7 @@ plugin_init(Plugin *plugin)
 		add_callbacks(plugin, callbacks);
 
 	/* remember which plugins are active */
-	active_plugin_list = g_list_append(active_plugin_list, plugin);
+	active_plugin_list = g_list_prepend(active_plugin_list, plugin);
 
 	geany_debug("Loaded:   %s (%s)", plugin->filename,
 		NVL(plugin->info.name, "<Unknown>"));
@@ -639,7 +639,7 @@ plugin_new(const gchar *fname, gboolean init_plugin, gboolean add_to_list)
 	{
 		geany_debug("Plugin \"%s\" already loaded.", fname);
 		if (add_to_list)
-			plugin_list = g_list_append(plugin_list, plugin);
+			plugin_list = g_list_prepend(plugin_list, plugin);
 		return plugin;
 	}
 
@@ -727,7 +727,7 @@ plugin_new(const gchar *fname, gboolean init_plugin, gboolean add_to_list)
 		plugin_init(plugin);
 
 	if (add_to_list)
-		plugin_list = g_list_append(plugin_list, plugin);
+		plugin_list = g_list_prepend(plugin_list, plugin);
 
 	return plugin;
 }
@@ -814,7 +814,7 @@ load_active_plugins(void)
 		if (NZV(fname) && g_file_test(fname, G_FILE_TEST_EXISTS))
 		{
 			if (plugin_new(fname, TRUE, FALSE) == NULL)
-				failed_plugins_list = g_list_append(failed_plugins_list, g_strdup(fname));
+				failed_plugins_list = g_list_prepend(failed_plugins_list, g_strdup(fname));
 		}
 	}
 }

plugins/filebrowser.c
@@ -492,8 +492,9 @@ static void open_selected_files(GList *list, gboolean do_not_focus)
 		GtkTreePath *treepath = item->data;
 		gchar *fname = get_tree_path_filename(treepath);
 
-		files = g_slist_append(files, fname);
+		files = g_slist_prepend(files, fname);
 	}
+	files = g_slist_reverse(files);
 	document_open_files(files, FALSE, NULL, NULL);
 	doc = document_get_current();
 	if (doc != NULL && ! do_not_focus)

src/editor.c
@@ -2538,7 +2538,7 @@ static gssize snippets_make_replacements(GeanyEditor *editor, GString *pattern,
 		if (i++ > 0)
 		{
 			cursor_steps += (nl_count * indent_size);
-			temp_list = g_list_append(temp_list, GINT_TO_POINTER(cursor_steps - old_cursor));
+			temp_list = g_list_prepend(temp_list, GINT_TO_POINTER(cursor_steps - old_cursor));
 		}
 		else
 		{
@@ -2563,6 +2563,7 @@ static gssize snippets_make_replacements(GeanyEditor *editor, GString *pattern,
 	{
 		GList *node;
 
+		temp_list = g_list_reverse(temp_list);
 		foreach_list(node, temp_list)
 			g_queue_push_nth(snippet_offsets, node->data, i++);
 

src/symbols.c
@@ -494,7 +494,7 @@ static GList *get_tag_list(GeanyDocument *doc, guint tag_types)
 
 		if (tag->type & tag_types)
 		{
-			tag_names = g_list_append(tag_names, tag);
+			tag_names = g_list_prepend(tag_names, tag);
 		}
 	}
 	tag_names = g_list_sort(tag_names, compare_symbol_lines);

src/tools.c
@@ -366,11 +366,12 @@ static void cc_show_dialog_custom_commands(void)
 			/* if the content of the entry is non-empty, add it to the result array */
 			if (text[0] != '\0')
 			{
-				result_list = g_slist_append(result_list, g_strdup(text));
+				result_list = g_slist_prepend(result_list, g_strdup(text));
 				len++;
 			}
 			g_list_free(list);
 		}
+		result_list = g_slist_reverse(result_list);
 		/* create a new null-terminated array but only if there any commands defined */
 		if (len > 0)
 		{

src/utils.c
@@ -1469,13 +1469,15 @@ GSList *utils_get_file_list_full(const gchar *path, gboolean full_path, gboolean
 
 	foreach_dir(filename, dir)
 	{
-		list = g_slist_append(list, full_path ?
+		list = g_slist_prepend(list, full_path ?
 			g_build_path(G_DIR_SEPARATOR_S, path, filename, NULL) : g_strdup(filename));
 	}
 	g_dir_close(dir);
 	/* sorting last is quicker than on insertion */
 	if (sort)
 		list = g_slist_sort(list, (GCompareFunc) utils_str_casecmp);
+	else
+		list = g_slist_reverse(list);
 	return list;
 }
 

commit 7a2a413f8c4eb3207d6d8c6502252d441d3c00a4

Restore sorting order of plugins configuration tabs (broke by me in r5586)
Now simply insert elements in sorted order rather than sorting the
list at a later point: it's simpler, cleaner and probably faster.

git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@5591 ea778897-0a13-0410-b9d1-a72fbfd435f5

src/plugins.c
@@ -554,6 +554,15 @@ static void read_key_group(Plugin *plugin)
 }
 
 
+static gint cmp_plugin_names(gconstpointer a, gconstpointer b)
+{
+	const Plugin *pa = a;
+	const Plugin *pb = b;
+
+	return strcmp(pa->info.name, pb->info.name);
+}
+
+
 static void
 plugin_init(Plugin *plugin)
 {
@@ -607,8 +616,10 @@ plugin_init(Plugin *plugin)
 	if (callbacks)
 		add_callbacks(plugin, callbacks);
 
-	/* remember which plugins are active */
-	active_plugin_list = g_list_prepend(active_plugin_list, plugin);
+	/* remember which plugins are active.
+	 * keep list sorted so tools menu items and plugin preference tabs are
+	 * sorted by plugin name */
+	active_plugin_list = g_list_insert_sorted(active_plugin_list, plugin, cmp_plugin_names);
 
 	geany_debug("Loaded:   %s (%s)", plugin->filename,
 		NVL(plugin->info.name, "<Unknown>"));
@@ -938,15 +949,6 @@ void plugins_load_active(void)
 }
 
 
-static gint cmp_plugin_names(gconstpointer a, gconstpointer b)
-{
-	const Plugin *pa = a;
-	const Plugin *pb = b;
-
-	return strcmp(pa->info.name, pb->info.name);
-}
-
-
 static void update_active_plugins_pref(void)
 {
 	gint i = 0;
@@ -960,10 +962,6 @@ static void update_active_plugins_pref(void)
 		active_plugins_pref = NULL;
 		return;
 	}
-
-	/* sort the list so next time tools menu items are sorted by plugin name
-	 * (not ideal to do here, but better than nothing) */
-	active_plugin_list = g_list_sort(active_plugin_list, cmp_plugin_names);
 
 	active_plugins_pref = g_new0(gchar*, count + 1);
 