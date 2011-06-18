1fa30c3a6568ef2fe5a7fe4270b6da77a2b34db8
Colomban Wendling <ban@herbesfolles.org>
2011-06-18 14:54:50 +0000

    Fix plugin function signatures after r5857 changes (no API/ABI changes) (oops)
    git-svn-id: https://geany.svn.sourceforge.net/svnroot/geany/trunk@5859 ea778897-0a13-0410-b9d1-a72fbfd435f5

--- src/plugindata.h
+++ src/plugindata.h
@@ -681,20 +681,20 @@ typedef struct StashFuncs
 	gint (*stash_group_save_to_file)(struct StashGroup *group, const gchar *filename,
 			GKeyFileFlags flags);
 	void (*stash_group_add_toggle_button)(struct StashGroup *group, gboolean *setting,
-			const gchar *key_name, gboolean default_value, gpointer widget_id);
+			const gchar *key_name, gboolean default_value, gconstpointer widget_id);
 	void (*stash_group_add_radio_buttons)(struct StashGroup *group, gint *setting,
 			const gchar *key_name, gint default_value,
-			gpointer widget_id, gint enum_id, ...) G_GNUC_NULL_TERMINATED;
+			gconstpointer widget_id, gint enum_id, ...) G_GNUC_NULL_TERMINATED;
 	void (*stash_group_add_spin_button_integer)(struct StashGroup *group, gint *setting,
-			const gchar *key_name, gint default_value, gpointer widget_id);
+			const gchar *key_name, gint default_value, gconstpointer widget_id);
 	void (*stash_group_add_combo_box)(struct StashGroup *group, gint *setting,
-			const gchar *key_name, gint default_value, gpointer widget_id);
+			const gchar *key_name, gint default_value, gconstpointer widget_id);
 	void (*stash_group_add_combo_box_entry)(struct StashGroup *group, gchar **setting,
-			const gchar *key_name, const gchar *default_value, gpointer widget_id);
+			const gchar *key_name, const gchar *default_value, gconstpointer widget_id);
 	void (*stash_group_add_entry)(struct StashGroup *group, gchar **setting,
-			const gchar *key_name, const gchar *default_value, gpointer widget_id);
+			const gchar *key_name, const gchar *default_value, gconstpointer widget_id);
 	void (*stash_group_add_widget_property)(struct StashGroup *group, gpointer setting,
-			const gchar *key_name, gpointer default_value, gpointer widget_id,
+			const gchar *key_name, gpointer default_value, gconstpointer widget_id,
 			const gchar *property_name, GType type);
 	void (*stash_group_display)(struct StashGroup *group, GtkWidget *owner);
 	void (*stash_group_update)(struct StashGroup *group, GtkWidget *owner);
