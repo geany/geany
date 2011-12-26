7c348e6847d017c90216e6177ba444dc1919a3cf
Colomban Wendling <ban@herbesfolles.org>
2011-12-26 19:14:55 +0100

    Fix "vergeany" -> "version" typo in some license headers
    Honestly, WTF?

--- src/geanyentryaction.h
+++ src/geanyentryaction.h
@@ -6,8 +6,8 @@
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either vergeany 2 of the License, or
- *      (at your option) any later vergeany.
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
  *
  *      This program is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
