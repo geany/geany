geany/geany

commit ca922e0ddc8022283ec3c1f49aaa15ab7c5ba213

Remove duplicated lists and name them more sensibly

data/geany.glade
@@ -668,7 +668,7 @@
     <property name="stock">gtk-help</property>
     <property name="icon-size">1</property>
   </object>
-  <object class="GtkListStore" id="liststore1">
+  <object class="GtkListStore" id="tab_pos_list">
     <columns>
       <!-- column-name item -->
       <column type="gchararray"/>
@@ -688,47 +688,7 @@
       </row>
     </data>
   </object>
-  <object class="GtkListStore" id="liststore2">
-    <columns>
-      <!-- column-name item -->
-      <column type="gchararray"/>
-    </columns>
-    <data>
-      <row>
-        <col id="0" translatable="yes">Left</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Right</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Top</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Bottom</col>
-      </row>
-    </data>
-  </object>
-  <object class="GtkListStore" id="liststore3">
-    <columns>
-      <!-- column-name item -->
-      <column type="gchararray"/>
-    </columns>
-    <data>
-      <row>
-        <col id="0" translatable="yes">Left</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Right</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Top</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Bottom</col>
-      </row>
-    </data>
-  </object>
-  <object class="GtkListStore" id="liststore4">
+  <object class="GtkListStore" id="indent_mode_list">
     <columns>
       <!-- column-name item -->
       <column type="gchararray"/>
@@ -748,41 +708,9 @@
       </row>
     </data>
   </object>
-  <object class="GtkListStore" id="liststore5">
-    <columns>
-      <!-- column-name item -->
-      <column type="gchararray"/>
-    </columns>
-    <data>
-      <row>
-        <col id="0" translatable="yes">None</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Basic</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Current chars</col>
-      </row>
-      <row>
-        <col id="0" translatable="yes">Match braces</col>
-      </row>
-    </data>
-  </object>
-  <object class="GtkListStore" id="liststore6">
+  <object class="GtkListStore" id="empty_chararray_list">
     <columns>
       <!-- column-name text -->
-      <column type="gchararray"/>
-    </columns>
-  </object>
-  <object class="GtkListStore" id="liststore7">
-    <columns>
-      <!-- column-name text -->
-      <column type="gchararray"/>
-    </columns>
-  </object>
-  <object class="GtkListStore" id="liststore8">
-    <columns>
-      <!-- column-name item -->
       <column type="gchararray"/>
     </columns>
   </object>
@@ -2176,7 +2104,7 @@
                                   <object class="GtkComboBox" id="combo_tab_msgwin">
                                     <property name="visible">True</property>
                                     <property name="can_focus">True</property>
-                                    <property name="model">liststore1</property>
+                                    <property name="model">tab_pos_list</property>
                                     <child>
                                       <object class="GtkCellRendererText" id="cellrenderertext1"/>
                                       <attributes>
@@ -2211,7 +2139,7 @@
                                   <object class="GtkComboBox" id="combo_tab_sidebar">
                                     <property name="visible">True</property>
                                     <property name="can_focus">True</property>
-                                    <property name="model">liststore1</property>
+                                    <property name="model">tab_pos_list</property>
                                     <child>
                                       <object class="GtkCellRendererText" id="cellrenderertext2"/>
                                       <attributes>
@@ -2244,7 +2172,7 @@
                                   <object class="GtkComboBox" id="combo_tab_editor">
                                     <property name="visible">True</property>
                                     <property name="can_focus">True</property>
-                                    <property name="model">liststore1</property>
+                                    <property name="model">tab_pos_list</property>
                                     <child>
                                       <object class="GtkCellRendererText" id="cellrenderertext3"/>
                                       <attributes>
@@ -3075,7 +3003,7 @@
                                       <object class="GtkComboBox" id="combo_auto_indent_mode">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">liststore4</property>
+                                        <property name="model">indent_mode_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext4"/>
                                           <attributes>
@@ -4246,7 +4174,7 @@
                                       <object class="GtkComboBox" id="combo_eol">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">liststore8</property>
+                                        <property name="model">empty_chararray_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext5"/>
                                           <attributes>
@@ -4348,7 +4276,7 @@
                                       <object class="GtkComboBox" id="combo_new_encoding">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">liststore6</property>
+                                        <property name="model">empty_chararray_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext6"/>
                                           <attributes>
@@ -4414,7 +4342,7 @@
                                       <object class="GtkComboBox" id="combo_open_encoding">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">liststore7</property>
+                                        <property name="model">empty_chararray_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext7"/>
                                           <attributes>
@@ -6472,7 +6400,7 @@
                   <object class="GtkComboBox" id="combo_auto_indent_mode_project">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="model">liststore5</property>
+                    <property name="model">indent_mode_list</property>
                     <child>
                       <object class="GtkCellRendererText" id="cellrenderertext1_project"/>
                       <attributes>

commit 7f16b9a4fb3af08239c928df3e634abd6af9ff16

Fix content of encodings and EOL style prefs ComboBoxes (oops)
I broke them in ca922e0ddc8022283ec3c1f49aaa15ab7c5ba213

Now the two encoding combo boxes shares the same data, which is
built only once.

data/geany.glade
@@ -440,7 +440,13 @@
       </object>
     </child>
   </object>
-  <object class="GtkListStore" id="empty_chararray_list">
+  <object class="GtkListStore" id="encoding_list">
+    <columns>
+      <!-- column-name text -->
+      <column type="gchararray"/>
+    </columns>
+  </object>
+  <object class="GtkListStore" id="eol_list">
     <columns>
       <!-- column-name text -->
       <column type="gchararray"/>
@@ -4157,7 +4163,7 @@
                                       <object class="GtkComboBox" id="combo_eol">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">empty_chararray_list</property>
+                                        <property name="model">eol_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext5"/>
                                           <attributes>
@@ -4259,7 +4265,7 @@
                                       <object class="GtkComboBox" id="combo_new_encoding">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">empty_chararray_list</property>
+                                        <property name="model">encoding_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext6"/>
                                           <attributes>
@@ -4325,7 +4331,7 @@
                                       <object class="GtkComboBox" id="combo_open_encoding">
                                         <property name="visible">True</property>
                                         <property name="can_focus">False</property>
-                                        <property name="model">empty_chararray_list</property>
+                                        <property name="model">encoding_list</property>
                                         <child>
                                           <object class="GtkCellRendererText" id="cellrenderertext7"/>
                                           <attributes>

src/prefs.c
@@ -1589,11 +1589,21 @@ static gboolean prefs_dialog_key_press_response_cb(GtkWidget *dialog, GdkEventKe
 }
 
 
+static void list_store_append_text(GtkListStore *list, const gchar *text)
+{
+	GtkTreeIter iter;
+
+	gtk_list_store_append(list, &iter);
+	gtk_list_store_set(list, &iter, 0, text, -1);
+}
+
+
 void prefs_show_dialog(void)
 {
 	if (ui_widgets.prefs_dialog == NULL)
 	{
-		GtkWidget *combo_new, *combo_open, *combo_eol;
+		GtkWidget *combo_new, *combo_open;
+		GtkListStore *encoding_list, *eol_list;
 		GtkWidget *label;
 		guint i;
 		gchar *encoding_string;
@@ -1606,24 +1616,24 @@ void prefs_show_dialog(void)
 		gtk_window_set_icon(GTK_WINDOW(ui_widgets.prefs_dialog), pb);
 		g_object_unref(pb);	/* free our reference */
 
-		/* init the default file encoding combo box */
+		/* init the file encoding combo boxes */
 		combo_new = ui_lookup_widget(ui_widgets.prefs_dialog, "combo_new_encoding");
 		combo_open = ui_lookup_widget(ui_widgets.prefs_dialog, "combo_open_encoding");
 		gtk_combo_box_set_wrap_width(GTK_COMBO_BOX(combo_new), 3);
 		gtk_combo_box_set_wrap_width(GTK_COMBO_BOX(combo_open), 3);
+		encoding_list = ui_builder_get_object("encoding_list");
 		for (i = 0; i < GEANY_ENCODINGS_MAX; i++)
 		{
 			encoding_string = encodings_to_string(&encodings[i]);
-			gtk_combo_box_append_text(GTK_COMBO_BOX(combo_new), encoding_string);
-			gtk_combo_box_append_text(GTK_COMBO_BOX(combo_open), encoding_string);
+			list_store_append_text(encoding_list, encoding_string);
 			g_free(encoding_string);
 		}
 
 		/* init the eol character combo box */
-		combo_eol = ui_lookup_widget(ui_widgets.prefs_dialog, "combo_eol");
-		gtk_combo_box_append_text(GTK_COMBO_BOX(combo_eol), utils_get_eol_name(SC_EOL_CRLF));
-		gtk_combo_box_append_text(GTK_COMBO_BOX(combo_eol), utils_get_eol_name(SC_EOL_CR));
-		gtk_combo_box_append_text(GTK_COMBO_BOX(combo_eol), utils_get_eol_name(SC_EOL_LF));
+		eol_list = ui_builder_get_object("eol_list");
+		list_store_append_text(eol_list, utils_get_eol_name(SC_EOL_CRLF));
+		list_store_append_text(eol_list, utils_get_eol_name(SC_EOL_CR));
+		list_store_append_text(eol_list, utils_get_eol_name(SC_EOL_LF));
 
 		/* add manually GeanyWrapLabels because they can't be added with Glade */
 		/* page Tools */