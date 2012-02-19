ca9dca94f890ed53741b2ec0b7bbdccaaeb72d36
Jiří Techet <techet@gmail.com>
2012-02-20 00:06:24 +0100

    Drop 'already' from the message in project close confirmation dialog
    Suppose you have project A open and want to open project B. Then the message
    saying "The 'A' project is already open" displays. This is slightly confusing
    and feels like if you were trying to re-open project A even though you
    are opening different project. The message without 'already' looks clearer
    in this context.

--- src/project.c
+++ src/project.c
@@ -594,7 +594,7 @@ gboolean project_ask_close(void)
 	{
 		if (dialogs_show_question_full(NULL, GTK_STOCK_CLOSE, GTK_STOCK_CANCEL,
 			_("Do you want to close it before proceeding?"),
-			_("The '%s' project is already open."), app->project->name))
+			_("The '%s' project is open."), app->project->name))
 		{
 			project_close(FALSE);
 			return TRUE;
