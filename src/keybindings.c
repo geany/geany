ddedc4408c61b0f67b3c64e9f2ced053703210c4
Eugene Arshinov <earshinov@gmail.com>
2012-02-23 23:54:52 +0400

    join_lines: Don't exclude trailing newline from the selection in `reflow_lines` (oops)
    This is already in by the calling code.

--- src/keybindings.c
+++ src/keybindings.c
@@ -2049,14 +2049,7 @@ static gint reflow_get_breaking_column(GeanyEditor *editor)
 
 static void reflow_lines(GeanyEditor *editor, gint column)
 {
-	gint start, indent, linescount, i, end;
-	gchar c;
-	ScintillaObject *sci = editor->sci;
-
-	/* don't include trailing newlines */
-	end = sci_get_selection_end(sci);
-	while ((c = sci_get_char_at(sci, end - 1)) == '\n' || c == '\r') end--;
-	sci_set_selection_end(sci, end);
+	gint start, indent, linescount, i;
 
 	start = sci_get_line_from_position(editor->sci,
 		sci_get_selection_start(editor->sci));
