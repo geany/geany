effab1a1291300018e8320b5a8c291c0ccb61cf4
Frank Lanitz <frank@frank.uvena.de>
2012-03-09 14:29:02 +0100

    Remove svn specific scripts as the are not needed anymore

--- scripts/svn-add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-FILES=$*
-if [ -n "$FILES" ]; then
-	svn add $FILES
-	svn propset svn:keywords 'Author Date Id Revision' $FILES
-	svn propset svn:eol-style native $FILES
-fi
-echo '>>> Remember to update Makefile.am, makefile.win32, wscript, po/POTFILES.in, geany.nsi (if necessary) <<<'
