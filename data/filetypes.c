7d097ca7ad7fe1cdd044fa83472facac091ce220
Matthew Brush <matt@geany.org>
2012-06-09 19:48:13 -0700

    Update 'docComment' keywords in C, C++, Vala, and Genie filetypes
    C and C++ have Doxygen keywords from:
        http://www.stack.nl/~dimitri/doxygen/commands.html
    
    Vala and Genie have Doxygen keywords and Valadoc keywords from:
        http://valadoc.org/#!wiki=markup

--- data/filetypes.c
+++ data/filetypes.c
@@ -28,8 +28,8 @@ tripleverbatim=string_2
 # all items must be in one line
 primary=asm auto break case char const continue default do double else enum extern float for goto if inline int long register restrict return short signed sizeof static struct switch typedef union unsigned void volatile while FALSE NULL TRUE
 secondary=
-# these are some doxygen keywords (incomplete)
-docComment=attention author brief bug class code date def enum example exception file fn namespace note param remarks return see since struct throw todo typedef var version warning union
+# these are the Doxygen keywords
+docComment=a addindex addtogroup anchor arg attention author authors b brief bug c callergraph callgraph category cite class code cond copybrief copydetails copydoc copyright date def defgroup deprecated details dir dontinclude dot dotfile e else elseif em endcode endcond enddot endhtmlonly endif endinternal endlatexonly endlink endmanonly endmsc endrtfonly endverbatim endxmlonly enum example exception extends file fn headerfile hideinitializer htmlinclude htmlonly if ifnot image implements include includelineno ingroup interface internal invariant latexonly li line link mainpage manonly memberof msc mscfile n name namespace nosubgrouping note overload p package page par paragraph param post pre private privatesection property protected protectedsection protocol public publicsection ref related relatedalso relates relatesalso remark remarks result return returns retval rtfonly sa section see short showinitializer since skip skipline snippet struct subpage subsection subsubsection tableofcontents test throw throws todo tparam typedef union until var verbatim verbinclude version warning weakgroup xmlonly xrefitem
 
 [lexer_properties]
 styling.within.preprocessor=1
