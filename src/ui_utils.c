593fef7fa0b9c799c58c1e8e9b36193572333b6f
Colomban Wendling <ban@herbesfolles.org>
2012-09-11 17:12:35 +0200

    Don't access GtkWidget:parent directly

--- src/ui_utils.c
+++ src/ui_utils.c
@@ -2439,7 +2439,7 @@ GtkWidget *ui_lookup_widget(GtkWidget *widget, const gchar *widget_name)
 		if (GTK_IS_MENU(widget))
 			parent = gtk_menu_get_attach_widget(GTK_MENU(widget));
 		else
-			parent = widget->parent;
+			parent = gtk_widget_get_parent(widget);
 		if (parent == NULL)
 			parent = (GtkWidget*) g_object_get_data(G_OBJECT(widget), "GladeParentKey");
 		if (parent == NULL)
