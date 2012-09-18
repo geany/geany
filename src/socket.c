f979b191cd0a0369b503a1aba5efc875447578da
YosefOr <yosefor3@walla.com>
2012-09-19 00:47:49 +0300

    Update src/socket.c
    http://developer.gnome.org/gtk3/3.4/ch24s02.html#id1458849

--- src/socket.c
+++ src/socket.c
@@ -653,8 +653,8 @@ gboolean socket_lock_input_cb(GIOChannel *source, GIOCondition condition, gpoint
 #ifdef G_OS_WIN32
 		else if (strncmp(buf, "window", 6) == 0)
 		{
-			HWND hwnd = (HWND) gdk_win32_drawable_get_handle(
-				GDK_DRAWABLE(gtk_widget_get_window(window)));
+			HWND hwnd = (HWND) gdk_win32_window_get_handle(
+				GDK_WINDOW(gtk_widget_get_window(window)));
 			socket_fd_write(sock, (gchar *)&hwnd, sizeof(hwnd));
 		}
 #endif
