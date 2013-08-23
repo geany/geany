08a1e4a0250b2f5419535fdbecd05ea2839555d1
Colomban Wendling <ban@herbesfolles.org>
2013-08-23 20:16:11 +0200

    Rust: fix file header

--- tagmanager/ctags/rust.c
+++ tagmanager/ctags/rust.c
@@ -4,8 +4,7 @@
 *   This source code is released for free distribution under the terms of the
 *   GNU General Public License.
 *
-*   This module contains functions for generating tags for Objective C
-*   language files.
+*   This module contains functions for generating tags for Rust files.
 */
 /*
 *   INCLUDE FILES
