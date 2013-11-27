31d20bf58b973bacb38a0397cc809e01ac55901b
Kamil Chmielewski <kamil.chm@gmail.com>
2013-11-28 00:08:59 +0100

    CTags for types, procedures and top level variables

--- tests/ctags/simple.nim
+++ tests/ctags/simple.nim
@@ -1,21 +1,25 @@
 # simple single line comment
 from somemodule import proc1, proc2
+import module3
 
-#type TWord = object of TObject
-#  word: string
-#  count: int
+type TWord = object of TObject
+  word: string
+  count: int
 
-#proc countWord(par: string): seq[TWord] =
+type TComplexWord = object of TWord
+  tag: string
+
+proc countWord(par: string): seq[TWord] =
   ## count words in paragraph
-#  result = @[]
-#  for word in split(par):
-#    result.add(TWord(count: 1, word: word))
-#  return result
+  result = @[]
+  var underFunction = "someString"
+  for word in split(par):
+    result.add(TWord(count: 1, word: word))
+  return result
 
-#const str1 = "simple string"
-#var str2 = """"Triple string"""
-#let str3 = r"raw string"
-
-#while p != nil and p.name != "xyz":
-#  # p.name is not evaluated if p == nil
-#  p = p.next
+const str1 = "simple string"
+var str2 = """Triple string"""
+let str3 = r"raw string"
+if True:
+  let str4 = "aaa"
+let someInt = 999
