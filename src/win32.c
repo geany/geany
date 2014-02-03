5c893e9b0eac95cc26f35a5dc279872ff1588f27
Colomban Wendling <ban@herbesfolles.org>
2014-02-03 15:36:28 +0100

    Windows: fix spawning commands with spaces
    Fix spawnning command under Windows when they contain spaces in both
    the executable and the arguments.  Apparently system() quoting is
    unexpected, and doesn't work properly if there is more than 2 quotes
    in the whole command.
    
    To work this around, spawn the command through `cmd.exe /S /C`.

--- src/win32.c
+++ src/win32.c
@@ -1050,7 +1050,7 @@ gboolean win32_spawn(const gchar *dir, gchar **argv, gchar **env, GSpawnFlags fl
 		return FALSE;
 	}
 	command = g_strjoinv(" ", argv);
-	SETPTR(command, g_strdup_printf("%s >%s 2>%s",
+	SETPTR(command, g_strdup_printf("cmd.exe /S /C \"%s >%s 2>%s\"",
 		command, tmp_file, tmp_errfile));
 	g_chdir(dir);
 	ret = system(command);
