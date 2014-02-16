c0d9328c0cd463db61f581cc9c14b3e1358413cb
Federico Reghenzani <federico@reghe.net>
2014-02-16 13:56:37 +0100

    Move new keybinds to the end of enum and update API number.

--- src/keybindings.h
+++ src/keybindings.h
@@ -126,10 +126,6 @@ enum GeanyKeyBindingID
 	GEANY_KEYS_BUILD_MAKE,						/**< Keybinding. */
 	GEANY_KEYS_INSERT_ALTWHITESPACE,			/**< Keybinding. */
 	GEANY_KEYS_EDITOR_SCROLLLINEDOWN,			/**< Keybinding. */
-	GEANY_KEYS_EDITOR_LINEUP,					/**< Keybinding. */
-	GEANY_KEYS_EDITOR_LINEDOWN,					/**< Keybinding. */
-	GEANY_KEYS_EDITOR_CHARRIGHT,					/**< Keybinding. */
-	GEANY_KEYS_EDITOR_CHARLEFT,					/**< Keybinding. */
 	GEANY_KEYS_VIEW_TOGGLEALL,					/**< Keybinding. */
 	GEANY_KEYS_VIEW_FULLSCREEN,					/**< Keybinding. */
 	GEANY_KEYS_GOTO_LINEEND,					/**< Keybinding. */
@@ -252,6 +248,10 @@ enum GeanyKeyBindingID
 	GEANY_KEYS_GOTO_LINESTARTVISUAL,			/**< Keybinding. */
 	GEANY_KEYS_DOCUMENT_CLONE,					/**< Keybinding. */
 	GEANY_KEYS_FILE_QUIT,						/**< Keybinding. */
+	GEANY_KEYS_EDITOR_LINEUP,					/**< Keybinding. */
+	GEANY_KEYS_EDITOR_LINEDOWN,					/**< Keybinding. */
+	GEANY_KEYS_EDITOR_CHARRIGHT,					/**< Keybinding. */
+	GEANY_KEYS_EDITOR_CHARLEFT,					/**< Keybinding. */
 	GEANY_KEYS_COUNT	/* must not be used by plugins */
 };
 
