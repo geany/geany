48f15a31c05f3cf25573d5547b5f5ee7cf92eb49
Devyn Collier Johnson <DevynCJohnson@Gmail.com>
2014-07-29 09:27:29 -0400

    Create main3.pyx

---
+++ data/templates/files/main3.pyx
@@ -0,0 +1,18 @@
+#!python3
+#cython: language_level=3, boundscheck=False
+#cython: c_string_encoding=utf-8, c_string_type=unicode
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+import pyximport
+pyximport.install()
+
+def int main():
+    """DOCSTRING"""
+    return 0
+
+cdef int FUNCTION():
+    """DOCSTRING"""
+    return 0
+
+if __name__ == "__main__":
+    main()
