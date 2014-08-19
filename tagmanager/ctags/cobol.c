acfd3c741b49fe768998dc6ee600726170e816c7
swconnolly <stephen@jsidata.ca>
2014-08-19 13:58:14 -0400

    Fixes for tag processing of COBOL files
      Updated the current tag parser of COBOL files to better handle the
    various keywords and to suppress false positives of various types.
    
    NOTE: code folding for COBOL files only works if the sequence number
    area is dropped , the indicator area begins in column 1 and those
    statements designated for area A begin in column 2. As such, the tag
    parsing is set up to deal with that type of file structure. The test for
    COBOL tagging has been changed to reflect this.

--- tagmanager/ctags/cobol.c
+++ tagmanager/ctags/cobol.c
@@ -20,17 +20,17 @@
 
 static void installCobolRegex (const langType language)
 {
-   addTagRegex (language, "^[ \t]*[0-9]+[ \t]+([A-Z0-9][A-Z0-9-]*)[ \t]+(BLANK|OCCURS|IS|JUST|PIC|REDEFINES|RENAMES|SIGN|SYNC|USAGE|VALUE)",
+   addTagRegex (language, "^[ \t]+[0-9]+[ \t]+(?!PIC|FILLER|SIGNED-INT|SIGNED-LONG|SIGNED-SHORT|UNSIGNED-INT|UNSIGNED-LONG|UNSIGNED-SHORT)([A-Z0-9][A-Z0-9-]+)[ \t]+(BLANK|OCCURS|IS|JUST|PIC|REDEFINES|RENAMES|SIGN|SYNC|USAGE|VALUE|SIGNED-INT|SIGNED-LONG|SIGNED-SHORT|UNSIGNED-INT|UNSIGNED-LONG|UNSIGNED-SHORT)",
 		"\\1", "d,variable,data items", "i");
-	addTagRegex (language, "^[ \t]*[FSR]D[ \t]+([A-Z0-9][A-Z0-9-]*)\\.",
+	addTagRegex (language, "^[ \t]{1,4}[FSR]D[ \t]+([A-Z0-9][A-Z0-9-]*)\\.{0,1}",
 		"\\1", "f,function,file descriptions (FD, SD, RD)", "i");
-	addTagRegex (language, "^[ \t]*[0-9]+[ \t]+([A-Z0-9][A-Z0-9-]*)\\.",
+	addTagRegex (language, "^[ \t]{1,4}[0-9]+[ \t]+(?!PIC|FILLER|SIGNED-INT|SIGNED-LONG|SIGNED-SHORT|UNSIGNED-INT|UNSIGNED-LONG|UNSIGNED-SHORT)([A-Z0-9][A-Z0-9-]*)\\.",
 		"\\1", "g,struct,group items", "i");
-	addTagRegex (language, "^[ \t]*([A-Z0-9][A-Z0-9-]*)\\.",
+	addTagRegex (language, "^[ \t]{1,4}(?!AUTHOR|PROGRAM-ID|FILE-CONTROL|I-O-CONTROL|OBJECT-COMPUTER|SOURCE-COMPUTER|DATE-COMPILED|SPECIAL-NAMES)([A-Z0-9][A-Z0-9-]*)\\.",
 		"\\1", "p,macro,paragraphs", "i");
-	addTagRegex (language, "^[ \t]*PROGRAM-ID\\.[ \t]+([A-Z0-9][A-Z0-9-]*)\\.",
+	addTagRegex (language, "^[ \t]{1,4}PROGRAM-ID\\.[ \t]+([A-Z0-9][A-Z0-9-]*)\\.",
 		"\\1", "P,class,program ids", "i");
-	addTagRegex (language, "^[ \t]*([A-Z0-9][A-Z0-9-]*)[ \t]+SECTION\\.",
+	addTagRegex (language, "^[ \t]{1,4}([A-Z0-9][A-Z0-9-]*)[ \t]+SECTION\\.",
 		"\\1", "n,namespace,sections", "i");
 }
 
