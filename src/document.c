geany/geany

commit b646424ddb715382a937d233a75bc684c22e18ec

Only close reload infobar if the response succeeded

src/document.c
@@ -3218,7 +3218,6 @@ static GtkWidget* document_show_message(GeanyDocument *doc, GtkMessageType msgty
 	g_free(markup);
 
 	g_signal_connect(info_widget, "response", G_CALLBACK(response_cb), doc);
-	g_signal_connect_after(info_widget, "response", G_CALLBACK(gtk_widget_destroy), NULL);
 
 	hbox = gtk_hbox_new(FALSE, 12);
 	gtk_box_pack_start(GTK_BOX(content_area), hbox, TRUE, TRUE, 0);
@@ -3266,15 +3265,28 @@ static GtkWidget* document_show_message(GeanyDocument *doc, GtkMessageType msgty
 
 static void on_monitor_reload_file_response(GtkWidget *bar, gint response_id, GeanyDocument *doc)
 {
+	gboolean close = FALSE;
+
+	// disable info bar so actions complete normally
 	unprotect_document(doc);
 	doc->priv->info_bars[MSG_TYPE_RELOAD] = NULL;
 
 	if (response_id == RESPONSE_DOCUMENT_RELOAD)
-		document_reload_prompt(doc, doc->encoding);
+		close = document_reload_prompt(doc, doc->encoding);
 	else if (response_id == RESPONSE_DOCUMENT_SAVE)
-		document_save_file(doc, TRUE);
+		close = document_save_file(doc, TRUE);	// force overwrite
 	else if (response_id == GTK_RESPONSE_CANCEL)
+	{
 		document_set_text_changed(doc, TRUE);
+		close = TRUE;
+	}
+	if (!close)
+	{
+		doc->priv->info_bars[MSG_TYPE_RELOAD] = bar;
+		protect_document(doc);
+		return;
+	}
+	gtk_widget_destroy(bar);
 }
 
 

commit 67cc8faf4be7778cda8c2901785b038319c1f01a

Fix closing of the "missing file" infobar
The issue was introduced in b646424ddb715382a937d233a75bc684c22e18ec as
it removed the default handler that used to destroy the infobar.

Now, the infobar is properly closed, but only if the action succeeded.

Closes #375.

src/document.c
@@ -3358,12 +3358,23 @@ static void on_monitor_resave_missing_file_response(GtkWidget *bar,
                                                     gint response_id,
                                                     GeanyDocument *doc)
 {
+	gboolean close = TRUE;
+
 	unprotect_document(doc);
 
 	if (response_id == RESPONSE_DOCUMENT_SAVE)
-		dialogs_show_save_as();
+		close = dialogs_show_save_as();
 
-	doc->priv->info_bars[MSG_TYPE_RESAVE] = NULL;
+	if (close)
+	{
+		doc->priv->info_bars[MSG_TYPE_RESAVE] = NULL;
+		gtk_widget_destroy(bar);
+	}
+	else
+	{
+		/* protect back the document if save didn't occur */
+		protect_document(doc);
+	}
 }
 
 