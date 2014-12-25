371301a84da81fdcd894715eb58af0efccd3a523
Colomban Wendling <ban@herbesfolles.org>
2014-12-25 01:36:27 +0100

    c: Don't parse wchar_t as a keyword

--- tagmanager/ctags/c.c
+++ tagmanager/ctags/c.c
@@ -506,7 +506,7 @@ static const keywordDesc KeywordTable [] = {
 	{ "virtual",        KEYWORD_VIRTUAL,        { 0, 1, 1, 0, 1, 1, 0 } },
 	{ "void",           KEYWORD_VOID,           { 1, 1, 1, 1, 1, 1, 1 } },
 	{ "volatile",       KEYWORD_VOLATILE,       { 1, 1, 1, 1, 0, 0, 1 } },
-	{ "wchar_t",        KEYWORD_WCHAR_T,        { 1, 1, 1, 0, 0, 0, 1 } },
+	{ "wchar_t",        KEYWORD_WCHAR_T,        { 0, 1, 1, 0, 0, 0, 1 } },
 	{ "weak",           KEYWORD_WEAK,           { 0, 0, 0, 0, 0, 1, 0 } },
 	{ "while",          KEYWORD_WHILE,          { 1, 1, 1, 1, 0, 1, 1 } }
 };
