ce0fa6ffa17befa0969653a0312b38a5429f1d6e
Dimitar Zhekov <dimitar.zhekov@gmail.com>
2015-03-10 21:24:42 +0200

    Add a spawn module for Geany
    g_spawn_async_with_pipes() under Windows has various drawbacks:
    
    - There is no g_shell_parse_argv() for windows. It's not hard to write
      one, but the command line recreated by mscvrt may be wrong.
    - GLib converts the argument vector to UNICODE. For non-UTF8 arguments,
      the result is often "Invalid string in argument vector at %d: %s:
      Invalid byte sequence in conversion input" (YMMV). Our tools (make,
      grep, gcc, ...) are "ANSI", so converting to UNICODE and then back
      only causes problems.
    - For various reasons, GLib uses an intermediate program to start
      children (see gspawn-win32.c), with the side effect that the
      grandchildren output (such as make -> gcc) is not captured.
    - With non-blocking pipes, the g_io_add_watch() callbacks are never
      invoked, while with blocking pipes, g_io_channel_read_line() blocks.
    - Some smaller problems, explained in spawn.c as inline comments.
    
    The spawn module tries to fix these problems, and to provide easier
    APIs, which guarantee that the callbacks will synchronized as expected,
    and g_io_channel_read_line() will not be required, since even under
    Unix, it may buffer lines of unlimited length.
    
    This initial commit only adds the module source and header files, and
    includes it in the various build systems.
    
    You can see PR 274 for a long discussion about the module.

---
+++ src/spawn.h
@@ -0,0 +1,102 @@
+/*
+ *      spawn.h - this file is part of Geany, a fast and lightweight IDE
+ *
+ *      Copyright 2013 Dimitar Toshkov Zhekov <dimitar(dot)zhekov(at)gmail(dot)com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License along
+ *      with this program; if not, write to the Free Software Foundation, Inc.,
+ *      51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+#ifndef GEANY_SPAWN_H
+#define GEANY_SPAWN_H 1
+
+#include <glib.h>
+
+gboolean spawn_check_command(const gchar *command_line, gboolean execute, GError **error);
+
+gboolean spawn_kill_process(GPid pid, GError **error);
+
+gboolean spawn_async_with_pipes(const gchar *working_directory, const gchar *command_line,
+	gchar **argv, gchar **envp, GPid *child_pid, gint *stdin_fd, gint *stdout_fd,
+	gint *stderr_fd, GError **error);
+
+gboolean spawn_async(const gchar *working_directory, const gchar *command_line, gchar **argv,
+	gchar **envp, GPid *child_pid, GError **error);
+
+/** Flags passed to @c spawn_with_callbacks(), which see. */
+typedef enum
+{
+	SPAWN_ASYNC                = 0x00,  /**< Asynchronous execution [default]. */
+	SPAWN_SYNC                 = 0x01,  /**< Synchronous execution. */
+	/* buffering modes */
+	SPAWN_LINE_BUFFERED        = 0x00,  /**< stdout/stderr are line buffered [default]. */
+	SPAWN_STDOUT_UNBUFFERED    = 0x02,  /**< stdout is not buffered. */
+	SPAWN_STDERR_UNBUFFERED    = 0x04,  /**< stderr is not buffered. */
+	SPAWN_UNBUFFERED           = 0x06,  /**< stdout/stderr are line buffered. */
+	/* recursive modes */
+	SPAWN_STDIN_RECURSIVE      = 0x08,  /**< The stdin callback is recursive. */
+	SPAWN_STDOUT_RECURSIVE     = 0x10,  /**< The stdout callback is recursive. */
+	SPAWN_STDERR_RECURSIVE     = 0x20,  /**< The stderr callback is recursive. */
+	SPAWN_RECURSIVE            = 0x38   /**< All callbacks are recursive. */
+} SpawnFlags;
+
+/**
+ *  Specifies the type of function passed to @c spawn_with_callbacks() as stdout or stderr
+ *  callback.
+ *
+ *  In unbuffered mode, the @a string may contain nuls, while in line buffered mode, it may
+ *  contain only a single nul as a line termination character at @a string->len - 1. In all
+ *  cases, the @a string will be terminated with a nul character that is not part of the data
+ *  at @a string->len.
+ *
+ *  If @c G_IO_IN or @c G_IO_PRI are set, the @a string will contain at least one character.
+ *
+ *  @param string contains the child data if @c G_IO_IN or @c G_IO_PRI are set.
+ *  @param condition the I/O condition which has been satisfied.
+ *  @param data the passed to @c spawn_with_callbacks() with the callback.
+ */
+typedef void (*SpawnReadFunc)(GString *string, GIOCondition condition, gpointer data);
+
+gboolean spawn_with_callbacks(const gchar *working_directory, const gchar *command_line,
+	gchar **argv, gchar **envp, SpawnFlags spawn_flags, GIOFunc stdin_cb, gpointer stdin_data,
+	SpawnReadFunc stdout_cb, gpointer stdout_data, gsize stdout_max_length,
+	SpawnReadFunc stderr_cb, gpointer stderr_data, gsize stderr_max_length,
+	GChildWatchFunc exit_cb, gpointer exit_data, GPid *child_pid, GError **error);
+
+/** 
+ *  A simple structure used by @c spawn_write_data() to write data to a channel.
+ *  See @c spawn_write_data() for more information.
+ */
+typedef struct _SpawnWriteData
+{
+	const gchar *ptr;   /**< Pointer to the data. May be NULL if the size is 0. */
+	gsize size;         /**< Size of the data. */
+} SpawnWriteData;
+
+gboolean spawn_write_data(GIOChannel *channel, GIOCondition condition, SpawnWriteData *data);
+
+void spawn_get_exit_status_cb(GPid pid, gint status, gpointer exit_status);
+
+gboolean spawn_sync(const gchar *working_directory, const gchar *command_line, gchar **argv,
+	gchar **envp, SpawnWriteData *stdin_data, GString *stdout_data, GString *stderr_data,
+	gint *exit_status, GError **error);
+
+#ifdef G_OS_WIN32
+#define WIFEXITED(status) TRUE
+#define WEXITSTATUS(status) (status)
+#define WIFSIGNALLED(status) FALSE
+#endif /* G_OS_WIN32 */
+
+#endif  /* GEANY_SPAWN_H */
