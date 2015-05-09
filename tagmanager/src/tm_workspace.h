54a9dedf0f52f4376494c8fd396476aafc42873d
Jiří Techet <techet@gmail.com>
2015-05-09 16:10:06 +0200

    Get scope members only from corresponding tag arrays
    At the moment it can happen that even though a member is found in the
    currently edited file, the search at the end of the function finds
    the type inside another file. This typically happens for anonymous
    structs so e.g. for anon_struct_0{...} from the current file we get
    members from anon_struct_0{...} from all open documents plus gloabl tags.
    
    Search in an increasing "circle" - start with current file only (trying
    all possible types of the variable), continue with workspace array and
    finally, if not found, search in the global tags.

--- tagmanager/src/tm_workspace.h
+++ tagmanager/src/tm_workspace.h
@@ -60,8 +60,8 @@ gboolean tm_workspace_create_global_tags(const char *pre_process, const char **i
 GPtrArray *tm_workspace_find(const char *name, const char *scope, TMTagType type,
 	TMTagAttrType *attrs, gboolean partial, langType lang);
 
-GPtrArray *tm_workspace_find_scope_members(const GPtrArray *file_tags,
-                                           const char *scope_name, langType lang);
+GPtrArray *tm_workspace_find_scope_members (TMSourceFile *source_file, const char *name);
+
 
 void tm_workspace_add_source_file_noupdate(TMSourceFile *source_file);
 
