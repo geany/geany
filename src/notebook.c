db7a13b4781895546ea8e616e633ccda4cea4afd
Enrico Tröger <enrico.troeger@uvena.de>
2015-08-25 21:53:23 +0200

    Mark "Open in New Window" menu item as translatable

--- src/notebook.c
+++ src/notebook.c
@@ -448,7 +448,7 @@ static void show_tab_bar_popup_menu(GdkEventButton *event, GeanyDocument *doc)
 	gtk_widget_show(menu_item);
 	gtk_container_add(GTK_CONTAINER(menu), menu_item);
 
-	menu_item = ui_image_menu_item_new(GTK_STOCK_OPEN, "Open in New _Window");
+	menu_item = ui_image_menu_item_new(GTK_STOCK_OPEN, _("Open in New _Window"));
 	gtk_widget_show(menu_item);
 	gtk_container_add(GTK_CONTAINER(menu), menu_item);
 	g_signal_connect(menu_item, "activate",
