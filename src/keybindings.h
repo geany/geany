f0c2aef0513f4a8ee961c065cd231ee238f2908c
Thomas Sahlin <thomas.sahlin@invented.se>
2016-01-08 10:38:12 +0100

    Added keybindings for custom commands 4-9

--- src/keybindings.h
+++ src/keybindings.h
@@ -266,6 +266,12 @@ enum GeanyKeyBindingID
 	GEANY_KEYS_GOTO_LINESTARTVISUAL,			/**< Keybinding. */
 	GEANY_KEYS_DOCUMENT_CLONE,					/**< Keybinding. */
 	GEANY_KEYS_FILE_QUIT,						/**< Keybinding. */
+	GEANY_KEYS_FORMAT_SENDTOCMD4,				/**< Keybinding. */
+	GEANY_KEYS_FORMAT_SENDTOCMD5,				/**< Keybinding. */
+	GEANY_KEYS_FORMAT_SENDTOCMD6,				/**< Keybinding. */
+	GEANY_KEYS_FORMAT_SENDTOCMD7,				/**< Keybinding. */
+	GEANY_KEYS_FORMAT_SENDTOCMD8,				/**< Keybinding. */
+	GEANY_KEYS_FORMAT_SENDTOCMD9,				/**< Keybinding. */
 	GEANY_KEYS_COUNT	/* must not be used by plugins */
 };
 
