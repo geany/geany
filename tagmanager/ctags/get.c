universal-ctags/ctags

Use explicit character literal instead of numerical value

main/get.c
@@ -928,9 +928,9 @@ process:
 					 * 	"xxx(raw)xxx";
 					 *
 					 * which is perfectly valid (yet probably very unlikely). */
-					int prev = getNthPrevCFromInputFile (1, 0);
-					int prev2 = getNthPrevCFromInputFile (2, 0);
-					int prev3 = getNthPrevCFromInputFile (3, 0);
+					int prev = getNthPrevCFromInputFile (1, '\0');
+					int prev2 = getNthPrevCFromInputFile (2, '\0');
+					int prev3 = getNthPrevCFromInputFile (3, '\0');
 
 					if (! isident (prev) ||
 					    (! isident (prev2) && (prev == 'L' || prev == 'u' || prev == 'U')) ||

geany/geany

tagmanager/ctags/get.c
@@ -801,9 +801,9 @@ process:
 					 * 	"xxx(raw)xxx";
 					 *
 					 * which is perfectly valid (yet probably very unlikely). */
-					int prev = fileGetNthPrevC (1, 0);
-					int prev2 = fileGetNthPrevC (2, 0);
-					int prev3 = fileGetNthPrevC (3, 0);
+					int prev = fileGetNthPrevC (1, '\0');
+					int prev2 = fileGetNthPrevC (2, '\0');
+					int prev3 = fileGetNthPrevC (3, '\0');
 
 					if (! isident (prev) ||
 					    (! isident (prev2) && (prev == 'L' || prev == 'u' || prev == 'U')) ||