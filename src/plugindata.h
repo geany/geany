geany/geany

commit d4aab06f9ba8bc0534303ce4b59b837a687be848

Move some documentation to show up in API docs.
The Doxygen comments were on the non-public global filetypes_array and
filetypes_by_title variables instead of the GeanyData members which are
exposed to the plugin API and reference manual.

src/plugindata.h
@@ -188,7 +188,14 @@ typedef struct GeanyData
 	 *
 	 * @see documents. */
 	GPtrArray					*documents_array;
-	GPtrArray					*filetypes_array;	/**< Dynamic array of GeanyFiletype pointers. @elementtype{GeanyFiletype} */
+	/** Dynamic array of filetype pointers
+	 *
+	 * List the list is dynamically expanded for custom filetypes filetypes so don't expect
+	 * the list of known filetypes to be a constant.
+	 *
+	 * @elementtype{GeanyFiletype}
+	 */
+	GPtrArray					*filetypes_array;
 	struct GeanyPrefs			*prefs;				/**< General settings */
 	struct GeanyInterfacePrefs	*interface_prefs;	/**< Interface settings */
 	struct GeanyToolbarPrefs	*toolbar_prefs;		/**< Toolbar settings */
@@ -198,7 +205,12 @@ typedef struct GeanyData
 	struct GeanyToolPrefs		*tool_prefs;		/**< Tool settings */
 	struct GeanyTemplatePrefs	*template_prefs;	/**< Template settings */
 	gpointer					*_compat;			/* Remove field on next ABI break (abi-todo) */
-	GSList						*filetypes_by_title; /**< See filetypes.h#filetypes_by_title. */
+	/** List of filetype pointers sorted by name, but with @c filetypes_index(GEANY_FILETYPES_NONE)
+	 * first, as this is usually treated specially.
+	 * The list does not change (after filetypes have been initialized), so you can use
+	 * @code g_slist_nth_data(filetypes_by_title, n) @endcode and expect the same result at different times.
+	 * @see filetypes_get_sorted_by_name(). */
+	GSList						*filetypes_by_title;
 }
 GeanyData;
 

src/filetypes.c
@@ -55,22 +55,8 @@
 
 #define GEANY_FILETYPE_SEARCH_LINES 2 /* lines of file to search for filetype */
 
-/** Dynamic array of filetype pointers
- *
- * List the list is dynamically expanded for custom filetypes filetypes so don't expect
- * the list of known filetypes to be a constant.
- *
- * @elementtype{GeanyFiletype}
- * */
 GPtrArray *filetypes_array = NULL;
-
 static GHashTable *filetypes_hash = NULL;	/* Hash of filetype pointers based on name keys */
-
-/** List of filetype pointers sorted by name, but with @c filetypes_index(GEANY_FILETYPES_NONE)
- * first, as this is usually treated specially.
- * The list does not change (after filetypes have been initialized), so you can use
- * @code g_slist_nth_data(filetypes_by_title, n) @endcode and expect the same result at different times.
- * @see filetypes_get_sorted_by_name(). */
 GSList *filetypes_by_title = NULL;
 
 

commit 114e339a7eb6296f0ad8a0ea7764ea22dcea47c3

Restore element-type annotation for GeanyData::documents_array
Broken in #966.

src/plugindata.h
@@ -186,7 +186,10 @@ typedef struct GeanyData
 	 * One reason is that notebook tabs can be reordered.
 	 * Use @c document_get_from_page() to lookup a document from a notebook tab number.
 	 *
-	 * @see documents. */
+	 * @see documents.
+	 *
+	 * @elementtype{GeanyDocument}
+	 */
 	GPtrArray					*documents_array;
 	/** Dynamic array of filetype pointers
 	 *