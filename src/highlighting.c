d69d8d0f0fb6b62979e77f22ff04a7095c524d35
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2018-02-06 22:56:12 +0100

    Removed duplicate case.

--- src/highlighting.c
+++ src/highlighting.c
@@ -1619,7 +1619,6 @@ gboolean highlighting_is_string_style(gint lexer, gint style)
 
 		case SCLEX_BATCH:
 		case SCLEX_DIFF:
-		case SCLEX_LATEX:
 		case SCLEX_MAKEFILE:
 		case SCLEX_MARKDOWN:
 		case SCLEX_PROPERTIES:
