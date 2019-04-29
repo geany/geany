a4027f9f8fdf4ab9efc984f88b945c9911e583b6
Colomban Wendling <ban@herbesfolles.org>
2019-04-29 13:32:11 +0200

    scintilla: Fix GCC 4.8 support wrt noexcept move constructors
    X-Scintilla-Commit-ID: ee3fe56c6fe815781d02fbc7d55d9f15f9c439aa

--- scintilla/src/XPM.h
+++ scintilla/src/XPM.h
@@ -26,9 +26,9 @@ public:
 	explicit XPM(const char *textForm);
 	explicit XPM(const char *const *linesForm);
 	XPM(const XPM &) = default;
-	XPM(XPM &&) noexcept = default;
+	XPM(XPM &&) = default;
 	XPM &operator=(const XPM &) = default;
-	XPM &operator=(XPM &&) noexcept = default;
+	XPM &operator=(XPM &&) = default;
 	~XPM();
 	void Init(const char *textForm);
 	void Init(const char *const *linesForm);
@@ -53,9 +53,9 @@ public:
 	RGBAImage(int width_, int height_, float scale_, const unsigned char *pixels_);
 	explicit RGBAImage(const XPM &xpm);
 	RGBAImage(const RGBAImage &) = default;
-	RGBAImage(RGBAImage &&) noexcept = default;
+	RGBAImage(RGBAImage &&) = default;
 	RGBAImage &operator=(const RGBAImage &) = default;
-	RGBAImage &operator=(RGBAImage &&) noexcept = default;
+	RGBAImage &operator=(RGBAImage &&) = default;
 	virtual ~RGBAImage();
 	int GetHeight() const { return height; }
 	int GetWidth() const { return width; }
