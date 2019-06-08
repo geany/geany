81fcd99df2d3db93ee09f7debce07e73fa0eb984
Nick Treleaven <n@trelsoft.com>
2019-06-08 17:28:06 +0100

    Opening recent projects: Don't ask whether to close current project
    Unlikely to be done accidentally.

--- src/ui_utils.c
+++ src/ui_utils.c
@@ -1191,7 +1191,8 @@ static void recent_project_activate_cb(GtkMenuItem *menuitem, G_GNUC_UNUSED gpoi
 	gchar *utf8_filename = ui_menu_item_get_text(menuitem);
 	gchar *locale_filename = utils_get_locale_from_utf8(utf8_filename);
 
-	if (project_ask_close() && project_load_file_with_session(locale_filename))
+	if (app->project && !project_close(FALSE)) {}
+	else if (project_load_file_with_session(locale_filename))
 		recent_file_loaded(utf8_filename, recent_get_recent_projects());
 
 	g_free(locale_filename);
