f64374ec57c9e704b0f1d588d24edce2194da249
Enrico Tröger <enrico.troeger@uvena.de>
2021-04-04 23:21:14 +0200

    SaveActions: Fix memory leak and possible invalid access

--- plugins/saveactions.c
+++ plugins/saveactions.c
@@ -287,6 +287,7 @@ static void instantsave_document_new_cb(GObject *obj, GeanyDocument *doc, gpoint
 			ui_set_statusbar(TRUE, "%s", message);
 			g_warning("%s", message);
 			g_free(message);
+			g_free(new_filename);
 			return;
 		}
 
@@ -294,7 +295,7 @@ static void instantsave_document_new_cb(GObject *obj, GeanyDocument *doc, gpoint
 
 		doc->file_name = new_filename;
 
-		if (doc->file_type->id == GEANY_FILETYPES_NONE)
+		if (doc->file_type && doc->file_type->id == GEANY_FILETYPES_NONE)
 			document_set_filetype(doc, filetypes_lookup_by_name(instantsave_default_ft));
 
 		/* force saving the file to enable all the related actions(tab name, filetype, etc.) */
