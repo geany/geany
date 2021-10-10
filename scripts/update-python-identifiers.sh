36f7511173c251db14615a17af464cd63a75e98d
Enrico Tröger <enrico.troeger@uvena.de>
2021-10-10 16:42:15 +0200

    Generate Python identifiers only for Python 3
    Python 2 is EOL since almost two years now, so remove Python2 keywords
    and simplify the script to generate them.

--- scripts/update-python-identifiers.sh
+++ scripts/update-python-identifiers.sh
@@ -3,8 +3,8 @@
 # Author:  Colomban Wendling <colomban@geany.org>
 # License: GPL v2 or later
 #
-# Updates the `identifiers` entry in data/filetypes.python.
-# Requires both Python 2 and 3.
+# Updates the `identifiers` entry in data/filetypes.python.in.
+# Requires Python 3.
 
 set -e
 
@@ -12,13 +12,9 @@ file=data/filedefs/filetypes.python.in
 
 [ -f "$file" ]
 
-py_2_and_3() {
-  python2 "$@" && python3 "$@"
-}
-
 # sort_filter [exclude...]
 sort_filter() {
-  python -c '\
+  python3 -c '\
 from sys import stdin; \
 items=set(stdin.read().strip().split("\n")); \
 exclude=['"$(for a in "$@"; do printf "'%s', " "$a"; done)"']; \
@@ -26,8 +22,8 @@ print(" ".join(sorted([i for i in items if i not in exclude])))
 '
 }
 
-keywords=$(py_2_and_3 -c 'from keyword import kwlist; print("\n".join(kwlist))')
-builtins=$(py_2_and_3 -c 'print("\n".join(dir(__builtins__)))')
+keywords=$(python3 -c 'from keyword import kwlist; print("\n".join(kwlist))')
+builtins=$(python3 -c 'print("\n".join(dir(__builtins__)))')
 
 primary=$(echo "$keywords" | sort_filter)
 # builtins, but excluding keywords that are already listed in primary=
