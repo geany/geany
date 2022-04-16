9612a0a277926c7a3cf8435594227948279c265a
Jiří Techet <techet@gmail.com>
2022-04-16 19:51:00 +0200

    Add Prolog filetype support
    This patch adds basic Prolog support (only scintilla laxer, there's no
    ctags parser). I used swi-prolog for the compiler and run commands which
    I believe is the most commonly used prolog implementation.
    
    This patch contains keyword suggestions made by @rdipardo, thanks.

--- src/highlighting.c
+++ src/highlighting.c
@@ -1041,6 +1041,7 @@ void highlighting_init_styles(guint filetype_idx, GKeyFile *config, GKeyFile *co
 		init_styleset_case(PHP);
 		init_styleset_case(PO);
 		init_styleset_case(POWERSHELL);
+		init_styleset_case(PROLOG);
 		init_styleset_case(PYTHON);
 		init_styleset_case(R);
 		init_styleset_case(RAKU);
@@ -1134,6 +1135,7 @@ void highlighting_set_styles(ScintillaObject *sci, GeanyFiletype *ft)
 		styleset_case(PHP);
 		styleset_case(PO);
 		styleset_case(POWERSHELL);
+		styleset_case(PROLOG);
 		styleset_case(PYTHON);
 		styleset_case(R);
 		styleset_case(RAKU);
@@ -1646,6 +1648,15 @@ gboolean highlighting_is_string_style(gint lexer, gint style)
 			return (style == SCE_POWERSHELL_STRING ||
 				style == SCE_POWERSHELL_CHARACTER);
 
+		case SCLEX_VISUALPROLOG:
+			return (style == SCE_VISUALPROLOG_STRING ||
+				style == SCE_VISUALPROLOG_STRING_ESCAPE ||
+				style == SCE_VISUALPROLOG_STRING_ESCAPE_ERROR ||
+				style == SCE_VISUALPROLOG_STRING_EOL_OPEN ||
+				style == SCE_VISUALPROLOG_STRING_VERBATIM ||
+				style == SCE_VISUALPROLOG_STRING_VERBATIM_SPECIAL ||
+				style == SCE_VISUALPROLOG_STRING_VERBATIM_EOL);
+
 		case SCLEX_BATCH:
 		case SCLEX_DIFF:
 		case SCLEX_LATEX:
@@ -1894,6 +1905,12 @@ gboolean highlighting_is_comment_style(gint lexer, gint style)
 		case SCLEX_AU3:
 			return (style == SCE_AU3_COMMENT ||
 				style == SCE_AU3_COMMENTBLOCK);
+
+		case SCLEX_VISUALPROLOG:
+			return (style == SCE_VISUALPROLOG_COMMENT_BLOCK ||
+				style == SCE_VISUALPROLOG_COMMENT_LINE ||
+				style == SCE_VISUALPROLOG_COMMENT_KEY ||
+				style == SCE_VISUALPROLOG_COMMENT_KEY_ERROR);
 	}
 	return FALSE;
 }
