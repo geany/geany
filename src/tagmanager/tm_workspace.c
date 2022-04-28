98638827a4b8192bdfd4b42d7ef8fe7a22cf9d40
Jiří Techet <techet@gmail.com>
2022-04-29 01:57:07 +0200

    Revert "Allow local variable autocompletion also before variable declaration"
    This reverts commit 0c6d315431b7ae336ea16f8f2ec6e9ce84e3d462.

--- src/tagmanager/tm_workspace.c
+++ src/tagmanager/tm_workspace.c
@@ -689,9 +689,10 @@ static gboolean is_valid_tag(TMTag *tag,
 	guint current_line,
 	const gchar *current_scope)
 {
-	/* ignore local variables from other files/functions */
+	/* ignore local variables from other files/functions or after current line */
 	return !(tag->type & tm_tag_local_var_t) ||
 		(current_file == tag->file &&
+		 current_line >= tag->line &&
 		 g_strcmp0(current_scope, tag->scope) == 0);
 }
 
