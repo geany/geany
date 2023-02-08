eb6b7abcded1ebe9c24da81ef0c896648865d86d
Nick Treleaven <ntrel002@gmail.com>
2023-02-08 15:09:36 +0000

    Add recent filename on saving when real_path is not set
    real_path is set for any documents on disk.
    
    Don't add new document opened from CLI to recent files, because it
    doesn't exist yet.
    No need now to add recent file on Save As, because real_path is nulled
    before calling document_save_file.

--- src/libmain.c
+++ src/libmain.c
@@ -852,8 +852,6 @@ gboolean main_handle_filename(const gchar *locale_filename)
 			document_show_tab(doc);
 		else
 			doc = document_new_file(utf8_filename, NULL, NULL);
-		if (doc != NULL)
-			ui_add_recent_document(doc);
 		g_free(utf8_filename);
 		g_free(filename);
 		return TRUE;
