aaaf750eeb6ad019c12c40c0ab5d3518cf10113a
Nick Treleaven <ntrel002@gmail.com>
2023-05-01 16:15:40 +0100

    Sync templates.d with ctags PR

--- tests/ctags/templates.d
+++ tests/ctags/templates.d
@@ -14,14 +14,22 @@ void f(Foo!x);
 
 template each(alias fun = "a")
 {
-    // FIXME
-    int zero(alias a) = 0; // should be var not func
-    alias foo(T) = T; // T
-    enum isRangeUnaryIterable(R) = // R
-        is(typeof(unaryFun!fun(R.init.front)));
-
     template child(){}
     void tmethod()(){}
 }
 
+// FIXME
+int VT(alias a) = 0; // parsed as function
+/+
+// parsed as T:
+alias AT(T) = T;
+enum ET(T) = T.init;
++/
+
 mixin ImplementLength!source; // source too!
+
+// declaration templates
+interface IT(T){}
+struct ST(T){}
+union UT(T){}
+
