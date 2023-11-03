lordrasmus/geany-lsp

LSP interface for document symbol queries

src/lsp.c
@@ -21,9 +21,76 @@
 #include "lsp.h"
 
 
+typedef enum {
+	LspKindFile = 1,
+	LspKindModule,
+	LspKindNamespace,
+	LspKindPackage,
+	LspKindClass,
+	LspKindMethod,
+	LspKindProperty,
+	LspKindField,
+	LspKindConstructor,
+	LspKindEnum,
+	LspKindInterface,
+	LspKindFunction,
+	LspKindVariable,
+	LspKindConstant,
+	LspKindString,
+	LspKindNumber,
+	LspKindBoolean,
+	LspKindArray,
+	LspKindObject,
+	LspKindKey,
+	LspKindNull,
+	LspKindEnumMember,
+	LspKindStruct,
+	LspKindEvent,
+	LspKindOperator,
+	LspKindTypeParameter,
+	LSP_KIND_NUM = LspKindTypeParameter
+} LspSymbolKind;  /* note: enums different than in LspCompletionItemKind */
+
+
+static LspSymbolKind kind_icons[LSP_KIND_NUM] = {
+	TM_ICON_NAMESPACE,  /* LspKindFile */
+	TM_ICON_NAMESPACE,  /* LspKindModule */
+	TM_ICON_NAMESPACE,  /* LspKindNamespace */
+	TM_ICON_NAMESPACE,  /* LspKindPackage */
+	TM_ICON_CLASS,      /* LspKindClass */
+	TM_ICON_METHOD,     /* LspKindMethod */
+	TM_ICON_MEMBER,     /* LspKindProperty */
+	TM_ICON_MEMBER,     /* LspKindField */
+	TM_ICON_METHOD,     /* LspKindConstructor */
+	TM_ICON_STRUCT,     /* LspKindEnum */
+	TM_ICON_CLASS,      /* LspKindInterface */
+	TM_ICON_METHOD,     /* LspKindFunction */
+	TM_ICON_VAR,        /* LspKindVariable */
+	TM_ICON_MACRO,      /* LspKindConstant */
+	TM_ICON_OTHER,      /* LspKindString */
+	TM_ICON_OTHER,      /* LspKindNumber */
+	TM_ICON_OTHER,      /* LspKindBoolean */
+	TM_ICON_OTHER,      /* LspKindArray */
+	TM_ICON_OTHER,      /* LspKindObject */
+	TM_ICON_OTHER,      /* LspKindKey */
+	TM_ICON_OTHER,      /* LspKindNull */
+	TM_ICON_MEMBER,     /* LspKindEnumMember */
+	TM_ICON_STRUCT,     /* LspKindStruct */
+	TM_ICON_OTHER,      /* LspKindEvent */
+	TM_ICON_OTHER,      /* LspKindOperator */
+	TM_ICON_OTHER       /* LspKindTypeParameter */
+};
+
+
 gboolean func_return_false(GeanyDocument *doc)
 {
 	return FALSE;
+}
+
+
+GPtrArray *func_return_ptrarr(GeanyDocument *doc)
+{
+	return NULL;
 }
 
 
@@ -37,6 +104,11 @@ void func_args_doc_bool(GeanyDocument *doc, gboolean dummy)
 }
 
 
+void func_args_doc_symcallback_ptr(GeanyDocument *doc, LspSymbolRequestCallback callback, gpointer user_data)
+{
+}
+
+
 static Lsp dummy_lsp = {
 	.autocomplete_available = func_return_false,
 	.autocomplete_perform = func_args_doc,
@@ -46,6 +118,10 @@ static Lsp dummy_lsp = {
 
 	.goto_available = func_return_false,
 	.goto_perform = func_args_doc_bool,
+
+	.doc_symbols_available = func_return_false,
+	.doc_symbols_request = func_args_doc_symcallback_ptr,
+	.doc_symbols_get_cached = func_return_ptrarr
 };
 
 static Lsp *current_lsp = &dummy_lsp;
@@ -64,6 +140,14 @@ GEANY_API_SYMBOL
 void lsp_unregister(Lsp *lsp)
 {
 	current_lsp = &dummy_lsp;
+}
+
+
+guint lsp_get_symbols_icon_id(guint kind)
+{
+	if (kind >= LspKindFile && kind <= LSP_KIND_NUM)
+		return kind_icons[kind - 1];
+	return TM_ICON_STRUCT;
 }
 
 
@@ -104,4 +188,22 @@ gboolean lsp_goto_available(GeanyDocument *doc)
 void lsp_goto_perform(GeanyDocument *doc, gboolean definition)
 {
 	CALL_IF_EXISTS(goto_perform)(doc, definition);
+}
+
+
+gboolean lsp_doc_symbols_available(GeanyDocument *doc)
+{
+	return CALL_IF_EXISTS(doc_symbols_available)(doc);
+}
+
+
+void lsp_doc_symbols_request(GeanyDocument *doc, LspSymbolRequestCallback callback, gpointer user_data)
+{
+	CALL_IF_EXISTS(doc_symbols_request)(doc, callback, user_data);
+}
+
+
+GPtrArray *lsp_doc_symbols_get_cached(GeanyDocument *doc)
+{
+	return CALL_IF_EXISTS(doc_symbols_get_cached)(doc);
 }

geany/geany

Since the whole code of the symbol tree is quite complex, the idea
of the interface between Geany and LSP plugins is to reuse as much
of the Geany symbol tree code as possible. This means using the
TMTag structs by LSP plugins to pass the symbol data to Geany.
The main interface consists of 3 functions:
- lsp_doc_symbols_available(GeanyDocument *doc) - checking whether
  the plugin supports symbol tree queries for the document of the
  given file type
- void lsp_doc_symbols_request(GeanyDocument *doc,
  LspSymbolRequestCallback callback, gpointer user_data) - function
  performing asynchronous request to the LSP server to obtain
  symbols for the current document. Once available, callback is
  executed.
- GPtrArray *lsp_doc_symbols_get_cached(GeanyDocument *doc) - this
  function returns (synchronously) the symbols obtained by the last
  call of lsp_doc_symbols_request(). The return value of this function
  is an array of TMTag structures which are filled by the plugin
  as specified below.
TMTag:
- name - the name of the symbol
- type - the SymbolKind code as specified by LSP specification
- line - the line of the symbol
- arglist - the "detail" field of LSP document symbol (typically
  some form of function signature)
- scope - scope obtained from serializing the children field of
  LSP document symbol with the scope separator valid for the given
  filetype, obtained by symbols_get_context_separator()
In addition, there's a auxiliary function lsp_get_symbols_icon_id()
that maps LSP symbol kind (passed inside TMTag type member) to
a Geany symbol icon (TM_ICON_...).

src/lsp.c
@@ -21,9 +21,76 @@
 #include "lsp.h"
 
 
+typedef enum {
+	LspKindFile = 1,
+	LspKindModule,
+	LspKindNamespace,
+	LspKindPackage,
+	LspKindClass,
+	LspKindMethod,
+	LspKindProperty,
+	LspKindField,
+	LspKindConstructor,
+	LspKindEnum,
+	LspKindInterface,
+	LspKindFunction,
+	LspKindVariable,
+	LspKindConstant,
+	LspKindString,
+	LspKindNumber,
+	LspKindBoolean,
+	LspKindArray,
+	LspKindObject,
+	LspKindKey,
+	LspKindNull,
+	LspKindEnumMember,
+	LspKindStruct,
+	LspKindEvent,
+	LspKindOperator,
+	LspKindTypeParameter,
+	LSP_KIND_NUM = LspKindTypeParameter
+} LspSymbolKind;  /* note: enums different than in LspCompletionItemKind */
+
+
+static LspSymbolKind kind_icons[LSP_KIND_NUM] = {
+	TM_ICON_NAMESPACE,  /* LspKindFile */
+	TM_ICON_NAMESPACE,  /* LspKindModule */
+	TM_ICON_NAMESPACE,  /* LspKindNamespace */
+	TM_ICON_NAMESPACE,  /* LspKindPackage */
+	TM_ICON_CLASS,      /* LspKindClass */
+	TM_ICON_METHOD,     /* LspKindMethod */
+	TM_ICON_MEMBER,     /* LspKindProperty */
+	TM_ICON_MEMBER,     /* LspKindField */
+	TM_ICON_METHOD,     /* LspKindConstructor */
+	TM_ICON_STRUCT,     /* LspKindEnum */
+	TM_ICON_CLASS,      /* LspKindInterface */
+	TM_ICON_METHOD,     /* LspKindFunction */
+	TM_ICON_VAR,        /* LspKindVariable */
+	TM_ICON_MACRO,      /* LspKindConstant */
+	TM_ICON_OTHER,      /* LspKindString */
+	TM_ICON_OTHER,      /* LspKindNumber */
+	TM_ICON_OTHER,      /* LspKindBoolean */
+	TM_ICON_OTHER,      /* LspKindArray */
+	TM_ICON_OTHER,      /* LspKindObject */
+	TM_ICON_OTHER,      /* LspKindKey */
+	TM_ICON_OTHER,      /* LspKindNull */
+	TM_ICON_MEMBER,     /* LspKindEnumMember */
+	TM_ICON_STRUCT,     /* LspKindStruct */
+	TM_ICON_OTHER,      /* LspKindEvent */
+	TM_ICON_METHOD,     /* LspKindOperator */
+	TM_ICON_OTHER       /* LspKindTypeParameter */
+};
+
+
 gboolean func_return_false(GeanyDocument *doc)
 {
 	return FALSE;
+}
+
+
+GPtrArray *func_return_ptrarr(GeanyDocument *doc)
+{
+	return NULL;
 }
 
 
@@ -37,6 +104,11 @@ void func_args_doc_bool(GeanyDocument *doc, gboolean dummy)
 }
 
 
+void func_args_doc_symcallback_ptr(GeanyDocument *doc, LspSymbolRequestCallback callback, gpointer user_data)
+{
+}
+
+
 static Lsp dummy_lsp = {
 	.autocomplete_available = func_return_false,
 	.autocomplete_perform = func_args_doc,
@@ -46,6 +118,10 @@ static Lsp dummy_lsp = {
 
 	.goto_available = func_return_false,
 	.goto_perform = func_args_doc_bool,
+
+	.doc_symbols_available = func_return_false,
+	.doc_symbols_request = func_args_doc_symcallback_ptr,
+	.doc_symbols_get_cached = func_return_ptrarr
 };
 
 static Lsp *current_lsp = &dummy_lsp;
@@ -64,6 +140,14 @@ GEANY_API_SYMBOL
 void lsp_unregister(Lsp *lsp)
 {
 	current_lsp = &dummy_lsp;
+}
+
+
+guint lsp_get_symbols_icon_id(guint kind)
+{
+	if (kind >= LspKindFile && kind <= LSP_KIND_NUM)
+		return kind_icons[kind - 1];
+	return TM_ICON_STRUCT;
 }
 
 
@@ -104,4 +188,22 @@ gboolean lsp_goto_available(GeanyDocument *doc)
 void lsp_goto_perform(GeanyDocument *doc, gboolean definition)
 {
 	CALL_IF_EXISTS(goto_perform)(doc, definition);
+}
+
+
+gboolean lsp_doc_symbols_available(GeanyDocument *doc)
+{
+	return CALL_IF_EXISTS(doc_symbols_available)(doc);
+}
+
+
+void lsp_doc_symbols_request(GeanyDocument *doc, LspSymbolRequestCallback callback, gpointer user_data)
+{
+	CALL_IF_EXISTS(doc_symbols_request)(doc, callback, user_data);
+}
+
+
+GPtrArray *lsp_doc_symbols_get_cached(GeanyDocument *doc)
+{
+	return CALL_IF_EXISTS(doc_symbols_get_cached)(doc);
 }