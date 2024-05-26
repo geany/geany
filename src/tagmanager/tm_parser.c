ca9789473ad32a696d8f6bc712b4631bbf86f159
Jiří Techet <techet@gmail.com>
2024-05-26 23:17:58 +0200

    Map Matlab variables back to tm_tag_undef_t
    The parser makes tags for each assignment which is too noisy.

--- src/tagmanager/tm_parser.c
+++ src/tagmanager/tm_parser.c
@@ -669,13 +669,12 @@ static TMParserMapGroup group_FORTRAN[] = {
 
 static TMParserMapEntry map_MATLAB[] = {
 	{'f', tm_tag_function_t},  // function
-	{'v', tm_tag_variable_t},  // variable
+	{'v', tm_tag_undef_t},     // variable
 	{'c', tm_tag_class_t},     // class
 };
 static TMParserMapGroup group_MATLAB[] = {
 	{N_("Classes"), TM_ICON_CLASS, tm_tag_class_t},
 	{N_("Functions"), TM_ICON_METHOD, tm_tag_function_t},
-	{N_("Variables"), TM_ICON_VAR, tm_tag_variable_t},
 };
 
 #define map_CUDA map_C
