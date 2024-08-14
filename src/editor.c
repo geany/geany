f55f2f71f34a5d8efcfe658837c5223f21aef781
Jiří Techet <techet@gmail.com>
2024-08-14 11:04:15 +0200

    Add Dart support
    This PR adds support for the Dart language:
    1. It uses the Dart lexer which has been recently merged to lexilla
    2. The keywords are based on Notepad4 keywords
    3. Build menu settings is based on
       https://github.com/geany/geany/pull/3372

--- src/editor.c
+++ src/editor.c
@@ -1321,6 +1321,7 @@ static gboolean lexer_has_braces(ScintillaObject *sci)
 		case SCLEX_CIL:
 		case SCLEX_CPP:
 		case SCLEX_D:
+		case SCLEX_DART:
 		case SCLEX_HTML:	/* for PHP & JS */
 		case SCLEX_PHPSCRIPT:
 		case SCLEX_PASCAL:	/* for multiline comments? */
@@ -5146,6 +5147,7 @@ void editor_set_indentation_guides(GeanyEditor *editor)
 		case SCLEX_D:
 		case SCLEX_OCTAVE:
 		case SCLEX_RUST:
+		case SCLEX_DART:
 			mode = SC_IV_LOOKBOTH;
 			break;
 
